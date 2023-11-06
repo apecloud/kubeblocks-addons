3316aee93c2f16e8bc9fa534e53cf18a811f054d
L.Dongming <l.dongming@apecloud.com>
2023-11-06 17:41:52 +0800

    chore: remove extra colume and fix description

--- hack/gen-addons.sh
+++ hack/gen-addons.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 # Initialize table header
-echo "| NAME | VERSION | APP-VERSION | DESCRIPTION |"
-echo "| ---- | ------- | ---------- | ----------- |"
+echo "| NAME | DESCRIPTION |"
+echo "| ---- | ----------- |"
 
 # Traverse and sort directories
-for d in $(find . -type d ! -name "*cluster" | sort); do
+for d in $(find . -type d ! -name "*cluster" -not -name "common" -not -name "kblib" | sort); do
   # Check if Chart.yaml file exists
   if [[ -f "$d/Chart.yaml" ]]; then
     # Use yq to read fields
     name=$(yq e '.name' $d/Chart.yaml)
-    version=$(yq e '.version' $d/Chart.yaml)
-    appVersion=$(yq e '.appVersion' $d/Chart.yaml)
     description=$(yq e '.description' $d/Chart.yaml)
 
     # Output as Markdown table row
-    echo "| $name | $version | $appVersion | $description |"
+    echo "| $name | $description |"
   fi
 done
