9dc47a1bb05efdf96c789d0f820940d0de6299d2
L.Dongming <l.dongming@apecloud.com>
2023-11-13 16:52:31 +0800

    chore: improve description and add app-version for readme

--- hack/get-addons.sh
+++ hack/get-addons.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Initialize table header
-echo "| NAME | DESCRIPTION |"
-echo "| ---- | ----------- |"
+echo "| NAME | APP-VERSION | DESCRIPTION |"
+echo "| ---- | --------- | ----------- |"
 
 # Traverse and sort directories
-for d in $(find . -type d ! -name "*cluster" -not -name "common" -not -name "kblib" | sort); do
+for d in $(find . -type d ! -name "*cluster" -not -name "common" -not -name "kblib" -not -name "neonvm" | sort); do
   # Check if Chart.yaml file exists
   if [[ -f "$d/Chart.yaml" ]]; then
     # Use yq to read fields
     name=$(yq e '.name' $d/Chart.yaml)
     description=$(yq e '.description' $d/Chart.yaml)
+    appVersion=$(yq e '.appVersion' $d/Chart.yaml)
 
     # Output as Markdown table row
-    echo "| $name | $description |"
+    echo "| $name | $appVersion | $description"
   fi
 done
