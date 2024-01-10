22db25258ac2e8db18714bfa9ebe46d91f4d8214
wangyelei <yelei.wang@apecloud.com>
2024-01-10 20:04:31 +0800

    fix: compatible old backup

--- addons/mongodb/dataprotection/mongodump-restore.sh
+++ addons/mongodb/dataprotection/mongodump-restore.sh
@@ -4,4 +4,11 @@ export PATH="$PATH:$DP_DATASAFED_BIN_PATH"
 export DATASAFED_BACKEND_BASE_PATH="$DP_BACKUP_BASE_PATH"
 
 mongo_uri="mongodb://${DP_DB_HOST}:${DP_DB_PORT}"
-datasafed pull -d zstd "${DP_BACKUP_NAME}.archive.zst" - | mongorestore --archive --uri "${mongo_uri}" -u ${MONGODB_ROOT_USER} -p ${MONGODB_ROOT_PASSWORD} --authenticationDatabase admin
+
+backupFile="${DP_BACKUP_NAME}.archive.zst"
+if [ "$(datasafed list ${backupFile})" == "${backupFile}" ]; then
+   datasafed pull -d zstd "${backupFile}" - | mongorestore --archive --uri "${mongo_uri}" -u ${MONGODB_ROOT_USER} -p ${MONGODB_ROOT_PASSWORD} --authenticationDatabase admin
+else
+   datasafed pull "${DP_BACKUP_NAME}.archive" - | mongorestore --archive --gzip --uri "${mongo_uri}" -u ${MONGODB_ROOT_USER} -p ${MONGODB_ROOT_PASSWORD} --authenticationDatabase admin
+fi
+
