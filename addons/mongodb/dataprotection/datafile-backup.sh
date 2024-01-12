2966d48114598cab95c743a9e09be1d329429d19
zjx20 <x.zhou@apecloud.com>
2024-01-12 18:15:44 +0800

    fix: remove the -z argument from tar commands (#224)
    (cherry picked from commit a03c06faab919046ba12579a085d305be52ce49c)

--- addons/mongodb/dataprotection/datafile-backup.sh
+++ addons/mongodb/dataprotection/datafile-backup.sh
@@ -6,7 +6,7 @@ cd ${DATA_DIR}
 START_TIME=$(get_current_time)
 # TODO: flush data and locked write, otherwise data maybe inconsistent
 # NOTE: if files changed during taring, the exit code will be 1 when it ends.
-tar -czvf - ./ | datasafed push -z zstd - "${DP_BACKUP_NAME}.tar.zst"
+tar -cvf - ./ | datasafed push -z zstd - "${DP_BACKUP_NAME}.tar.zst"
 rm -rf mongodb.backup
 # stat and save the backup information
 stat_and_save_backup_info $START_TIME
