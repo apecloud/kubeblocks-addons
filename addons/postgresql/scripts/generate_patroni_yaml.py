844df2c3486f418a1dd83995e40c368cc8ee6476
L.Dongming <l.dongming@apecloud.com>
2024-01-20 13:34:10 +0800

    chore: support configure patroni parameters

--- addons/postgresql/scripts/generate_patroni_yaml.py
+++ addons/postgresql/scripts/generate_patroni_yaml.py
@@ -3,12 +3,16 @@
 import os
 import sys
 import yaml
+
+
 def write_file(config, filename, overwrite):
     if not overwrite and os.path.exists(filename):
         pass
     else:
         with open(filename, 'w') as f:
             f.write(config)
+
+
 def read_file_lines(file):
     ret = []
     for line in file.readlines():
@@ -16,6 +20,8 @@ def read_file_lines(file):
         if line and not line.startswith('#'):
             ret.append(line)
     return ret
+
+
 def postgresql_conf_to_dict(file_path):
     with open(file_path, 'r') as f:
         content = f.read()
@@ -29,33 +35,54 @@ def postgresql_conf_to_dict(file_path):
         key, value = line.split('=', 1)
         result[key.strip()] = value.strip().strip("'")
     return result
+
+
 def main(filename):
     restore_dir = os.environ.get('RESTORE_DATA_DIR', '')
     local_config = yaml.safe_load(
-        os.environ.get('SPILO_CONFIGURATION', os.environ.get('PATRONI_CONFIGURATION', ''))) or {}
-    if not 'postgresql' in local_config:
+        os.environ.get('SPILO_CONFIGURATION',
+                       os.environ.get('PATRONI_CONFIGURATION', ''))) or {}
+
+    # set postgresql parameters
+    if 'postgresql' not in local_config:
         local_config['postgresql'] = {}
     postgresql = local_config['postgresql']
     postgresql['config_dir'] = '/home/postgres/pgdata/conf'
     postgresql['custom_conf'] = '/home/postgres/conf/postgresql.conf'
+
     # add pg_hba.conf
     with open('/home/postgres/conf/pg_hba.conf', 'r') as f:
         lines = read_file_lines(f)
         if lines:
             postgresql['pg_hba'] = lines
-    if restore_dir and os.path.isfile(os.path.join(restore_dir, 'kb_restore.signal')):
-        if not 'bootstrap' in local_config:
+    if restore_dir and os.path.isfile(
+            os.path.join(restore_dir, 'kb_restore.signal')):
+        if 'bootstrap' not in local_config:
             local_config['bootstrap'] = {}
         with open('/home/postgres/conf/kb_restore.conf', 'r') as f:
             local_config['bootstrap'].update(yaml.safe_load(f))
+
     # point in time recovery(PITR)
-    data_dir = os.environ.get('PGDATA', '')
     if os.path.isfile("/home/postgres/pgdata/conf/recovery.conf"):
         with open('/home/postgres/conf/kb_pitr.conf', 'r') as f:
             pitr_config = yaml.safe_load(f)
             re_config = postgresql_conf_to_dict("/home/postgres/pgdata/conf/recovery.conf")
             pitr_config[pitr_config['method']]['recovery_conf'].update(re_config)
             local_config['bootstrap'].update(pitr_config)
-    write_file(yaml.dump(local_config, default_flow_style=False), filename, True)
+
+    # patroni parameters
+    if 'bootstrap' not in local_config:
+        local_config['bootstrap'] = {}
+    if 'dcs' not in local_config['bootstrap']:
+        local_config['bootstrap']['dcs'] = {}
+    if os.path.exists('/home/postgres/conf/patroni.yaml'):
+        with open('/home/postgres/conf/patroni.yaml', 'r') as f:
+            local_config['bootstrap']['dcs'].update(yaml.safe_load(f))
+    else:
+        print('patroni.yaml not found')
+    write_file(yaml.dump(local_config, default_flow_style=False), filename,
+               True)
+
+
 if __name__ == '__main__':
     main(sys.argv[1])
