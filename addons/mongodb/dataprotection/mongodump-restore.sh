a658c41460acd8948ba272183ddcbdf5fec1f01e
wangyelei <yelei.wang@apecloud.com>
2024-02-02 11:00:58 +0800

    chore: update mongodump scripts (#290)

--- addons/mongodb/dataprotection/mongodump-restore.sh
+++ addons/mongodb/dataprotection/mongodump-restore.sh
@@ -5,9 +5,14 @@ export DATASAFED_BACKEND_BASE_PATH="$DP_BACKUP_BASE_PATH"
 
 mongo_uri="mongodb://${DP_DB_HOST}:${DP_DB_PORT}"
 
+oplogFlags=""
+oplogSignal="oplog.signal"
+if [ "$(datasafed list ${oplogSignal})" == "${oplogSignal}" ];then
+  oplogFlags="--oplogReplay"
+fi
 backupFile="${DP_BACKUP_NAME}.archive.zst"
 if [ "$(datasafed list ${backupFile})" == "${backupFile}" ]; then
-   datasafed pull -d zstd-fastest "${backupFile}" - | mongorestore --archive --uri "${mongo_uri}" -u ${MONGODB_ROOT_USER} -p ${MONGODB_ROOT_PASSWORD} --authenticationDatabase admin
+   datasafed pull -d zstd-fastest "${backupFile}" - | mongorestore --archive ${oplogFlags} --uri "${mongo_uri}" -u ${MONGODB_ROOT_USER} -p ${MONGODB_ROOT_PASSWORD} --authenticationDatabase admin
 else
    datasafed pull "${DP_BACKUP_NAME}.archive" - | mongorestore --archive --gzip --uri "${mongo_uri}" -u ${MONGODB_ROOT_USER} -p ${MONGODB_ROOT_PASSWORD} --authenticationDatabase admin
 fi
