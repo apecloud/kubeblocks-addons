5b587be6af54ec6da060d59ed1028c877faa274a
Y-Rookie <yong.huang@apecloud.com>
2024-02-27 18:17:45 +0800

    fix typo

--- addons/camellia-redis-proxy/scripts/camellia-redis-proxy-start.sh
+++ addons/camellia-redis-proxy/scripts/camellia-redis-proxy-start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -ex
 
 java -XX:+UseG1GC -Xms4096m -Xmx4096m -Dio.netty.tryReflectionSetAccessible=true \
@@ -14,6 +14,6 @@ java -XX:+UseG1GC -Xms4096m -Xmx4096m -Dio.netty.tryReflectionSetAccessible=true
 --add-opens java.base/jdk.internal.access=ALL-UNNAMED \
 --add-opens java.base/jdk.internal.misc=ALL-UNNAMED \
 --add-opens java.base/sun.net.util=ALL-UNNAMED \
---spring.config.location="file:${CAMELLIA_REDIS_PROXY_APPLICATION_CONFIG},file:${CAMELLIA_REDIS_PROXY_PROPERTIES_CONFIG},file:${CAMELLIA_REDIS_PROXY_PROPERTIES_JSON_CONFIG},file:${CAMELLIA_REDIS_PROXY_BACKEND_RESOURCE_CONFIG}" \
+-Dspring.config.additional-location="file:${CAMELLIA_REDIS_PROXY_APPLICATION_CONFIG},file:${CAMELLIA_REDIS_PROXY_PROPERTIES_CONFIG},file:${CAMELLIA_REDIS_PROXY_PROPERTIES_JSON_CONFIG},file:${CAMELLIA_REDIS_PROXY_BACKEND_RESOURCE_CONFIG}" \
 -server org.springframework.boot.loader.launch.JarLauncher
 
