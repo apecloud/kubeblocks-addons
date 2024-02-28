d5374ec480490ae02133feec51bc8a027d76cd6c
wangyelei <yelei.wang@apecloud.com>
2024-02-28 23:12:35 +0800

    chore: fix mongodb backup failed when using hostnetwork

--- addons/mongodb/dataprotection/backup-info-collector.sh
+++ addons/mongodb/dataprotection/backup-info-collector.sh
@@ -1,6 +1,6 @@
 function get_current_time() {
   CLIENT=$(which mongosh >/dev/null && echo mongosh || echo mongo)
-  curr_time=$(${CLIENT} -u ${DP_DB_USER} -p ${DP_DB_PASSWORD} --port 27017 --host ${DP_DB_HOST} --authenticationDatabase admin --eval 'db.isMaster().lastWrite.lastWriteDate.getTime()/1000' --quiet)
+  curr_time=$(${CLIENT} -u ${DP_DB_USER} -p ${DP_DB_PASSWORD} --port ${DP_DB_PORT} --host ${DP_DB_HOST} --authenticationDatabase admin --eval 'db.isMaster().lastWrite.lastWriteDate.getTime()/1000' --quiet)
   curr_time=$(date -d "@${curr_time}" -u '+%Y-%m-%dT%H:%M:%SZ')
   echo $curr_time
 }
