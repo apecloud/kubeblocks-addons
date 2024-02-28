4386374c63e353ded5ebe4a71ddfbe6d683b19c5
Y-Rookie <yong.huang@apecloud.com>
2024-02-28 14:40:23 +0800

    fix start script

--- addons/camellia-redis-proxy/scripts/camellia-redis-proxy-start.sh
+++ addons/camellia-redis-proxy/scripts/camellia-redis-proxy-start.sh
@@ -2,6 +2,8 @@
 set -ex
 
 java -XX:+UseG1GC -Xms4096m -Xmx4096m -Dio.netty.tryReflectionSetAccessible=true \
+-Dspring.config.location="${CAMELLIA_REDIS_PROXY_APPLICATION_CONFIG}" \
+-Ddynamic.conf.file.path="${CAMELLIA_REDIS_PROXY_PROPERTIES_CONFIG}" \
 --add-opens java.base/java.lang=ALL-UNNAMED \
 --add-opens java.base/java.io=ALL-UNNAMED \
 --add-opens java.base/java.math=ALL-UNNAMED \
@@ -14,6 +16,5 @@ java -XX:+UseG1GC -Xms4096m -Xmx4096m -Dio.netty.tryReflectionSetAccessible=true
 --add-opens java.base/jdk.internal.access=ALL-UNNAMED \
 --add-opens java.base/jdk.internal.misc=ALL-UNNAMED \
 --add-opens java.base/sun.net.util=ALL-UNNAMED \
--Dspring.config.additional-location="file:${CAMELLIA_REDIS_PROXY_APPLICATION_CONFIG},file:${CAMELLIA_REDIS_PROXY_PROPERTIES_CONFIG},file:${CAMELLIA_REDIS_PROXY_PROPERTIES_JSON_CONFIG},file:${CAMELLIA_REDIS_PROXY_BACKEND_RESOURCE_CONFIG}" \
 -server org.springframework.boot.loader.launch.JarLauncher
 
