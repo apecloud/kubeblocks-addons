a63ddc5923680ac60351b5c2b1e0190c50e5c341
Y-Rookie <yong.huang@apecloud.com>
2024-04-22 16:49:30 +0800

    modify redis start script

--- addons/redis/scripts/redis-start.sh
+++ addons/redis/scripts/redis-start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -ex
 
 declare -g primary
@@ -11,51 +11,20 @@ extract_ordinal_from_object_name() {
   echo "$ordinal"
 }
 
-shutdown_redis_server() {
-  if [ -n "$REDIS_DEFAULT_PASSWORD" ]; then
-    redis-cli -h 127.0.0.1 -p "$service_port" -a "$REDIS_DEFAULT_PASSWORD" shutdown
-  else
-    redis-cli -h 127.0.0.1 -p "$service_port" shutdown
-  fi
-}
-
-check_redis_server_ready() {
-  if [ -n "$REDIS_DEFAULT_PASSWORD" ]; then
-    retry redis-cli -h 127.0.0.1 -p "$service_port" -a "$REDIS_DEFAULT_PASSWORD" ping
-  else
-    retry redis-cli -h 127.0.0.1 -p "$service_port" ping
-  fi
-}
-
-# usage: retry <command>
-retry() {
-  local max_attempts=20
-  local attempt=1
-  until "$@" || [ $attempt -eq $max_attempts ]; do
-    echo "Command '$*' failed. Attempt $attempt of $max_attempts. Retrying in 5 seconds..."
-    attempt=$((attempt + 1))
-    sleep 3
-  done
-  if [ $attempt -eq $max_attempts ]; then
-    echo "Command '$*' failed after $max_attempts attempts. shutdown redis-server..."
-    shutdown_redis_server
-  fi
-}
-
 load_redis_template_conf() {
   echo "include /etc/conf/redis.conf" >> /etc/redis/redis.conf
 }
 
 build_redis_default_accounts() {
-  if [ ! -z "$REDIS_REPL_PASSWORD" ]; then
+  if [ -n "$REDIS_REPL_PASSWORD" ]; then
     echo "masteruser $REDIS_REPL_USER" >> /etc/redis/redis.conf
     echo "masterauth $REDIS_REPL_PASSWORD" >> /etc/redis/redis.conf
     echo "user $REDIS_REPL_USER on +psync +replconf +ping >$REDIS_REPL_PASSWORD" >> /data/users.acl
   fi
-  if [ ! -z "$REDIS_SENTINEL_PASSWORD" ]; then
+  if [ -n "$REDIS_SENTINEL_PASSWORD" ]; then
     echo "user $REDIS_SENTINEL_USER on allchannels +multi +slaveof +ping +exec +subscribe +config|rewrite +role +publish +info +client|setname +client|kill +script|kill >$REDIS_SENTINEL_PASSWORD" >> /data/users.acl
   fi
-  if [ ! -z "$REDIS_DEFAULT_PASSWORD" ]; then
+  if [ -n "$REDIS_DEFAULT_PASSWORD" ]; then
     echo "protected-mode yes" >> /etc/redis/redis.conf
     echo "user default on >$REDIS_DEFAULT_PASSWORD ~* &* +@all " >> /data/users.acl
   else
@@ -79,7 +48,7 @@ build_announce_ip_and_port() {
 
 build_redis_service_port() {
   service_port=6379
-  if [ ! -z "$SERVICE_PORT" ]; then
+  if [ -n "$SERVICE_PORT" ]; then
     service_port=$SERVICE_PORT
   fi
   echo "port $service_port" >> /etc/redis/redis.conf
@@ -96,7 +65,6 @@ build_replicaof_config() {
 }
 
 rebuild_redis_acl_file() {
-  {{- $data_root := getVolumePathByName ( index $.podSpec.containers 0 ) "data" }}
   if [ -f /data/users.acl ]; then
     sed -i "/user default on/d" /data/users.acl
     sed -i "/user $REDIS_REPL_USER on/d" /data/users.acl
@@ -148,46 +116,12 @@ init_or_get_primary_node() {
   done
 }
 
-start_redis_server() {
-    exec redis-server /etc/redis/redis.conf \
-    --loadmodule /opt/redis-stack/lib/redisearch.so ${REDISEARCH_ARGS} \
-    --loadmodule /opt/redis-stack/lib/redisgraph.so ${REDISGRAPH_ARGS} \
-    --loadmodule /opt/redis-stack/lib/redistimeseries.so ${REDISTIMESERIES_ARGS} \
-    --loadmodule /opt/redis-stack/lib/rejson.so ${REDISJSON_ARGS} \
-    --loadmodule /opt/redis-stack/lib/redisbloom.so ${REDISBLOOM_ARGS}
-}
-
 parse_redis_advertised_svc_if_exist() {
-  local pod_name="$1"
-
-  if [[ -z "${REDIS_ADVERTISED_PORT}" ]]; then
-    echo "Environment variable REDIS_ADVERTISED_PORT not found. Ignoring."
-    return 0
-  fi
-
-  # the value format of REDIS_ADVERTISED_PORT is "pod1Svc:advertisedPort1,pod2Svc:advertisedPort2,..."
-  IFS=',' read -ra advertised_ports <<< "${REDIS_ADVERTISED_PORT}"
-
-  local found=false
-  pod_name_ordinal=$(extract_ordinal_from_object_name "$pod_name")
-  for advertised_port in "${advertised_ports[@]}"; do
-    IFS=':' read -ra parts <<< "$advertised_port"
-    local svc_name="${parts[0]}"
-    local port="${parts[1]}"
-    svc_name_ordinal=$(extract_ordinal_from_object_name "$svc_name")
-    if [[ "$svc_name_ordinal" == "$pod_name_ordinal" ]]; then
-      echo "Found matching svcName and port for podName '$pod_name', REDIS_ADVERTISED_PORT: $REDIS_ADVERTISED_PORT. svcName: $svc_name, port: $port."
-      redis_advertised_svc_port_value="$port"
-      redis_advertised_svc_host_value="$KB_HOST_IP"
-      found=true
-      break
-    fi
-  done
-
-  if [[ "$found" == false ]]; then
-    echo "Error: No matching svcName and port found for podName '$pod_name', REDIS_ADVERTISED_PORT: $REDIS_ADVERTISED_PORT. Exiting."
-    exit 1
-  fi
+  pod_ordinal=$(extract_ordinal_from_pod_name "$KB_POD_NAME")
+  gen_redis_node_port="REDIS_NODE_PORT_${pod_ordinal}"
+  eval redis_node_port_value="\$$gen_redis_node_port"
+  redis_node_port_host_value=$KB_HOST_IP
+  echo "redis_node_port_value=$redis_node_port_value, redis_node_port_host_value=$redis_node_port_host_value"
 }
 
 # build redis.conf
@@ -200,5 +134,15 @@ build_redis_conf() {
   build_redis_default_accounts
 }
 
+start_redis_server() {
+    exec redis-server /etc/redis/redis.conf \
+    --loadmodule /opt/redis-stack/lib/redisearch.so ${REDISEARCH_ARGS} \
+    --loadmodule /opt/redis-stack/lib/redisgraph.so ${REDISGRAPH_ARGS} \
+    --loadmodule /opt/redis-stack/lib/redistimeseries.so ${REDISTIMESERIES_ARGS} \
+    --loadmodule /opt/redis-stack/lib/rejson.so ${REDISJSON_ARGS} \
+    --loadmodule /opt/redis-stack/lib/redisbloom.so ${REDISBLOOM_ARGS}
+}
+
+parse_redis_advertised_svc_if_exist
 build_redis_conf
 start_redis_server
