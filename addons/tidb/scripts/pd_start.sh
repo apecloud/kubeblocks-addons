ed781acf6a90befc6989fd97c74176e9c8c379fc
Harold Cheng <niuchangcun@gmail.com>
2024-05-10 23:41:15 +0800

    pd scale out

--- addons/tidb/scripts/pd_start.sh
+++ addons/tidb/scripts/pd_start.sh
@@ -2,26 +2,53 @@
 
 set -exo pipefail
 
-echo "start pd..."
 # TODO: clusterDomain 'cluster.local' requires configurable
 DOMAIN=$KB_NAMESPACE".svc.cluster.local"
 MY_PEER=$KB_POD_FQDN".cluster.local"
-PEERS=""
-i=0
-while [ $i -lt $KB_REPLICA_COUNT ]; do
-    if [ $i -ne 0 ]; then
-    PEERS="$PEERS,";
-    fi;
-    host=$(eval echo \$KB_"$i"_HOSTNAME)
-    host=$host"."$DOMAIN
-    hostname=${KB_CLUSTER_COMP_NAME}-${i}
-    PEERS="$PEERS$hostname=http://$host:2380"
-    i=$(( i + 1))
-done
-exec /pd-server --name="${HOSTNAME}" \
-    --data-dir=/var/lib/pd \
+
+DATA_DIR="/var/lib/pd"
+ARGS="--name=$HOSTNAME \
+    --data-dir=$DATA_DIR \
     --peer-urls=http://0.0.0.0:2380 \
-    --advertise-peer-urls=http://"${MY_PEER}":2380 \
+    --advertise-peer-urls=http://$MY_PEER:2380 \
     --client-urls=http://0.0.0.0:2379 \
-    --advertise-client-urls=http://"${MY_PEER}":2379 \
-    --initial-cluster="${PEERS}"
+    --advertise-client-urls=http://$MY_PEER:2379"
+
+if [[ -f $DATA_DIR/join ]]; then
+    echo "restarted pod, join cluster"
+    join=$(cat $DATA_DIR/join | tr "," "\n" | awk -F'=' '{print $2}' | tr "\n" ",")
+    join=${join%,}
+    ARGS="${ARGS} --join=${join}"
+elif [[ ! -d $DATA_DIR/member/wal ]]; then
+    echo "first started pod"
+    if [[ -n $KB_LEADER || -n $KB_FOLLOWERS ]]; then
+        echo "joining an existing cluster"
+        join=""
+        i=0
+        while [ $i -lt $KB_REPLICA_COUNT ]; do
+            host=$(eval echo \$KB_"$i"_HOSTNAME)
+            host=$host"."$DOMAIN
+            join="${join}http://$host:2380,"
+            i=$(( i + 1))
+        done
+        join=${join%,}
+        ARGS="${ARGS} --join=${join}"
+    else
+        echo "initializing a cluster"
+        PEERS=""
+        i=0
+        while [ $i -lt $KB_REPLICA_COUNT ]; do
+            if [ $i -ne 0 ]; then
+                PEERS="$PEERS,";
+            fi;
+            host=$(eval echo \$KB_"$i"_HOSTNAME)
+            host=$host"."$DOMAIN
+            hostname=${KB_CLUSTER_COMP_NAME}-${i}
+            PEERS="$PEERS$hostname=http://$host:2380"
+            i=$(( i + 1))
+        done
+        ARGS="${ARGS} --initial-cluster=${PEERS}"
+    fi
+fi
+# restarted pod, initial cluster doesn't need --join or --initial-cluster args
+exec /pd-server ${ARGS}
