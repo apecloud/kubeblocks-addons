47c89325f50651b720fe54b89f88c7b98cd404c6
Harold Cheng <niuchangcun@gmail.com>
2024-05-13 23:51:02 +0800

    backup wip

---
+++ addons/tidb/dataprotection/backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -exo pipefail
+
+cat /etc/datasafed/datasafed.conf
+toolConfig=/etc/datasafed/datasafed.conf
+
+function getToolConfigValue() {
+    local var=$1
+    cat $toolConfig | grep "$var" | awk '{print $NF}'
+}
+
+access_key_id=$(getToolConfigValue access_key_id)
+secret_access_key=$(getToolConfigValue secret_access_key)
+endpoint=$(getToolConfigValue endpoint)
+bucket=$(getToolConfigValue root)
+
+/br backup full --pd "$DP_DB_HOST" --storage "s3://$bucket$DP_BACKUP_BASE_PATH?access-key=$access_key_id&secret-access-key=$secret_access_key" --s3.endpoint "$endpoint"
