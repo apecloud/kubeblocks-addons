1c889d3d635b4f735ab1967a1be5db455182c0fb
hanguobiao <guobiaohan2020@163.com>
2024-06-07 09:10:22 +0800

    feat: update cmpd and add tools dockerfile

--- addons/mysql/scripts/mysql-entrypoint.sh
+++ addons/mysql/scripts/mysql-entrypoint.sh
@@ -9,11 +9,14 @@ if [ "${MYSQL_MAJOR}" = '5.7' ]; then
     --plugin-load-add=audit_log=audit_log.so \
     --log-bin=/var/lib/mysql/binlog/${KB_POD_NAME}-bin \
     --skip-slave-start=$skip_slave_start
-else
+elif [ "${MYSQL_MAJOR}" = '8.0' ]; then
 docker-entrypoint.sh mysqld --server-id $SERVICE_ID --report-host ${REPORT_HOST} \
    --plugin-load-add=rpl_semi_sync_master=semisync_master.so \
    --plugin-load-add=rpl_semi_sync_slave=semisync_slave.so \
    --plugin-load-add=audit_log=audit_log.so \
    --log-bin=/var/lib/mysql/binlog/${KB_POD_NAME}-bin \
    --skip-slave-start=$skip_slave_start
+else
+  echo "Unsupported MySQL version"
+  exit 1
 fi
