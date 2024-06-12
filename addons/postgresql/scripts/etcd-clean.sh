d00fb0fa3c670bc583a88a8e31e0263578a6915f
skyrise-l <332437807@qq.com>
2024-06-12 18:27:04 +0800

    fix delete script

--- addons/postgresql/scripts/etcd-clean.sh
+++ addons/postgresql/scripts/etcd-clean.sh
@@ -4,6 +4,10 @@ if [ -z "$ETCD_SERVER" ]; then
   exit 0
 fi
 
+echo "Stopping Patroni processes..."
+pkill -f patroni
+sleep 3
+
 export ETCDCTL_API=${ETCD_API:-'2'}
 
 endpoints=${ETCD_SERVER:-'127.0.0.1:2379'}
@@ -20,7 +24,6 @@ done
 servers=${servers:1}
 
 echo $servers
-
 
 echo "Deleting all keys with prefix /service/${KB_CLUSTER_NAME}-${KB_COMP_NAME}-patroni${KB_CLUSTER_UID_POSTFIX_8} from Etcd server at ${endpoints}..."
 etcdctl --endpoints $servers del /service/${KB_CLUSTER_NAME}-${KB_COMP_NAME}-patroni${KB_CLUSTER_UID_POSTFIX_8} --prefix
