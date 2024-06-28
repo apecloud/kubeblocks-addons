57c3994f0ccd4c1a2de6e9f6062563e5923b65ba
Y-Rookie <yong.huang@apecloud.com>
2024-06-28 17:46:21 +0800

    compatible redis addon upgrade v0.8.3 to v0.9.0

--- addons/redis/scripts/redis-sentinel-setup.sh
+++ addons/redis/scripts/redis-sentinel-setup.sh
@@ -13,17 +13,25 @@ set -ex
 {{- $primary_pod := "" }}
 {{- range $i, $e := $.cluster.spec.componentSpecs }}
   {{- if index $e "componentDefRef" }}
-    {{- /* xxxx-7.0 suffix just for compatible config render do not panic, this script has been deprecated in KubeBlock v0.10 */}}
+    {{- /* xxxx-7 suffix just for compatible config render do not panic, this script has been deprecated in KubeBlock v0.10 */}}
     {{- if eq $e.componentDefRef "redis-sentinel-7" }}
       {{- $sentinel_component = $e }}
+    {{- else if eq $e.componentDefRef "redis-sentinel" }}
+      {{- $sentinel_component = $e }}
     {{- else if eq $e.componentDefRef "redis-7" }}
+      {{- $redis_component = $e }}
+    {{- else if eq $e.componentDefRef "redis" }}
       {{- $redis_component = $e }}
     {{- end }}
   {{- end }}
   {{- if index $e "componentDef" }}
     {{- if eq $e.componentDef "redis-sentinel-7" }}
       {{- $sentinel_component = $e }}
+    {{- else if eq $e.componentDef "redis-sentinel" }}
+      {{- $sentinel_component = $e }}
     {{- else if eq $e.componentDef "redis-7" }}
+      {{- $redis_component = $e }}
+    {{- else if eq $e.componentDef "redis" }}
       {{- $redis_component = $e }}
     {{- end }}
   {{- end }}
