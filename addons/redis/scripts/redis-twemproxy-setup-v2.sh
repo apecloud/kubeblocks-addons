7e5911ece148ca8103796f3dc54f9d0db75c1d51
Y-Rookie <yong.huang@apecloud.com>
2024-07-10 11:01:00 +0800

    update

--- addons/redis/scripts/redis-twemproxy-setup-v2.sh
+++ addons/redis/scripts/redis-twemproxy-setup-v2.sh
@@ -18,35 +18,45 @@ build_redis_twemproxy_conf() {
   fi
 
   # Convert REDIS_SERVICE_NAMES and REDIS_SERVICE_PORTS to arrays
-  # The format of REDIS_SERVICE_NAMES is "redis0:redis-redis0-redis,redis1:redis-redis1-redis"
-  # The format of REDIS_SERVICE_PORTS is "redis0:6379,redis1:6379"
+  # The format of REDIS_SERVICE_NAMES is "redis0:redis-redis0-redis,redis1:redis-redis1-redis" when there are multiple redis server shards
+  # The format of REDIS_SERVICE_PORTS is "redis0:6379,redis1:6379" when there are multiple redis server shards
+  # The format of REDIS_SERVICE_NAMES is "redis-redis0-redis" when there is only one redis server shard
+  # The format of REDIS_SERVICE_PORTS is "6379" when there is only one redis server shard
   service_names_array=$(convert_to_array "$REDIS_SERVICE_NAMES")
   service_ports_array=$(convert_to_array "$REDIS_SERVICE_PORTS")
   echo "service_names_array: $service_names_array, service_ports_array: $service_ports_array"
 
-  # Initialize servers variable
+  # Initialize servers configuration
   servers=""
-  for service_name_entry in $service_names_array; do
-    # Extract key and value from service_name_entry
-    service_name_key="${service_name_entry%%:*}"
-    service_name_value="${service_name_entry#*:}"
+  # shellcheck disable=SC2046
+  if [ $(echo "$service_names_array" | wc -w) -eq 1 ] && [ $(echo "$service_ports_array" | wc -w) -eq 1 ]; then
+    service_name="${service_names_array#*:}"
+    service_port="${service_ports_array#*:}"
+    servers="    - $service_name:$service_port:1"
+  else
+    echo "service_names_array: $service_names_array, service_ports_array: $service_ports_array"
+    for service_name_entry in $service_names_array; do
+      # Extract key and value from service_name_entry
+      service_name_key="${service_name_entry%%:*}"
+      service_name_value="${service_name_entry#*:}"
 
-    echo "service_name_key: $service_name_key, service_name_value: $service_name_value"
-    # Find the corresponding port entry
-    for service_port_entry in $service_ports_array; do
-      service_port_key="${service_port_entry%%:*}"
-      service_port_value="${service_port_entry#*:}"
-      echo "service_port_key: $service_port_key, service_port_value: $service_port_value"
+      echo "service_name_key: $service_name_key, service_name_value: $service_name_value"
+      # Find the corresponding port entry
+      for service_port_entry in $service_ports_array; do
+        service_port_key="${service_port_entry%%:*}"
+        service_port_value="${service_port_entry#*:}"
+        echo "service_port_key: $service_port_key, service_port_value: $service_port_value"
 
-      if [ "$service_name_key" = "$service_port_key" ]; then
-        # Append to servers string
-        if [ -n "$servers" ]; then
-          servers="$servers\n"
+        if [ "$service_name_key" = "$service_port_key" ]; then
+          # Append to servers string
+          if [ -n "$servers" ]; then
+            servers="$servers\n"
+          fi
+          servers="$servers    - $service_name_value:$service_port_value:1"
         fi
-        servers="$servers    - $service_name_value:$service_port_value:1"
-      fi
+      done
     done
-  done
+  fi
 
   # All the components' password of redis server must be the same, So we find the first environment variable that starts with REDIS_DEFAULT_PASSWORD
   REDIS_AUTH_PASSWORD=""
