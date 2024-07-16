26fcf2b771aa12bb30ff9d753b3459a2c7b32a83
Y-Rookie <yong.huang@apecloud.com>
2024-07-16 17:19:54 +0800

    optimize echo log

--- addons/redis/scripts/redis-preStop.sh
+++ addons/redis/scripts/redis-preStop.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 set -e
 if [ ! -z "$REDIS_DEFAULT_PASSWORD" ]; then
-  redis-cli -h 127.0.0.1 -p 6379 -a "$REDIS_DEFAULT_PASSWORD" acl save
+  acl_save_command="redis-cli -h 127.0.0.1 -p 6379 -a $REDIS_DEFAULT_PASSWORD acl save"
 else
-  redis-cli -h 127.0.0.1 -p 6379 acl save
+  acl_save_command="redis-cli -h 127.0.0.1 -p 6379 acl save"
+fi
+echo "acl save command: $acl_save_command" | sed "s/$REDIS_DEFAULT_PASSWORD/********/g"
+if output=$($acl_save_command 2>&1); then
+  echo "acl save command executed successfully: $output"
+else
+  echo "failed to execute acl save command: $output"
+  exit 1
 fi
--- addons/redis/scripts/redis-sentinel-post-start.sh
+++ addons/redis/scripts/redis-sentinel-post-start.sh
@@ -4,4 +4,5 @@ set -e
 if [ -n "$SENTINEL_PASSWORD" ]; then
   until redis-cli -h 127.0.0.1 -p 26379 -a $SENTINEL_PASSWORD ping; do sleep 1; done
   redis-cli -h 127.0.0.1 -p 26379 ACL SETUSER $SENTINEL_USER ON \>$SENTINEL_PASSWORD allchannels +@all
+  echo "redis sentinel user and password set successfully."
 fi
