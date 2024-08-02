cf11167e2a4abd4b04015242eaf3ac878d8cbdd4
DWJ-Squirtle <1797411565@qq.com>
2024-08-02 16:56:29 +0800

    fix

--- addons/redis/scripts/redis-sentinel-offline.sh
+++ addons/redis/scripts/redis-sentinel-offline.sh
@@ -2,9 +2,8 @@
 set -ex
 
 declare -g redis_default_service_port=26379
-declare -A master_slave_counts
 
-offline_sentinel() {
+memberLeave_sentinel() {
 
   if [ -z "$KB_LEAVE_MEMBER_POD_IP" ]; then
     echo "Error: Required environment variable KB_LEAVE_MEMBER_POD_IP is not set."
@@ -24,57 +23,25 @@ offline_sentinel() {
   read -ra sentinel_pod_list <<< "${KB_MEMBER_ADDRESSES}"
   set +f
   IFS="$old_ifs"
+
   for sentinel_pod in "${sentinel_pod_list[@]}"; do
     host=$(echo "$sentinel_pod" | cut -d ':' -f 1)
     port=$(echo "$sentinel_pod" | cut -d ':' -f 2)
     sentinel_name="${host%%.*}"
 
-    if [ -z "$port" ]; then
-      port="$redis_default_service_port"
+    if [ -n "$port" ]; then
+      redis_default_service_port="$port"
     fi
-
+    #TODO:check if there is an ongoing HA switchover Before executing the reset command
     if [ "$sentinel_name" != "$sentinel_leave_member_name" ]; then
       if [ -n "$SENTINEL_PASSWORD" ]; then
-        redis-cli -h "$host" -p "$port" -a "$SENTINEL_PASSWORD" sentinel reset "*"
+        redis-cli -h "$host" -p "$redis_default_service_port" -a "$SENTINEL_PASSWORD" sentinel reset "*"
       else
-        redis-cli -h "$host" -p "$port" sentinel reset "*"
-      fi
-      if [ -n "$SENTINEL_PASSWORD" ]; then
-        output=$(redis-cli -h "$host" -p "$port" -a "$SENTINEL_PASSWORD" sentinel masters)
-      else
-        output=$(redis-cli -h "$host" -p "$port" sentinel masters)
-      fi
-      if [[ -n "$output" ]]; then
-          master_name=""
-          num_slaves=""
-          while read -r line; do
-              case "$line" in
-                  name)
-                      read -r master_name
-                      ;;
-                  num-slaves)
-                      read -r num_slaves
-                      ;;
-              esac
-              if [[ -n "$master_name" && -n "$num_slaves" ]]; then
-                echo "Master Name: $master_name, num-slaves: $num_slaves"
-                if [[ -z "${master_slave_counts[$master_name]}" ]]; then
-                  master_slave_counts[$master_name]=$num_slaves
-                else
-                  if [[ "${master_slave_counts[$master_name]}" -ne "$num_slaves" ]]; then
-                    echo "The number of slaves does not match the previous count; reset failed."
-                    exit 1
-                  fi
-                fi
-              master_name=""
-              num_slaves=""
-              fi
-          done <<< "$output"
-      else
-          echo "unable to connect to Redis Sentinel, or no master nodes found."
+        redis-cli -h "$host" -p "$redis_default_service_port" sentinel reset "*"
       fi
     fi
+    #TODO: Check that all the Sentinels agree about the number of Sentinels currently active
   done
-  echo "reset successful"
 }
-offline_sentinel
+
+memberLeave_sentinel
