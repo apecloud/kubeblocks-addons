9628e8b97abab5c223ed47177a37ab21a91bf0c9
loomt <loomt_@outlook.com>
2024-08-09 18:09:26 +0800

    feat: etcd addon revamp (#825)

---
+++ addons/etcd/scripts/role-probe-script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+status=$(execEtcdctl 127.0.0.1:2379 endpoint status --command-timeout=300ms --dial-timeout=100m)
+IsLeader=$(echo $status | awk -F ', ' '{print $5}')
+IsLearner=$(echo $status | awk -F ', ' '{print $6}')
+
+if [ "true" = "$IsLeader" ]; then
+  echo -n "leader"
+elif [ "true" = "$IsLearner" ]; then
+  echo -n "learner"
+elif [ "false" = "$IsLeader" ] && [ "false" = "$IsLearner" ]; then
+  echo -n "follower"
+else
+  echo -n "bad role, please check!"
+  exit 1
+fi
