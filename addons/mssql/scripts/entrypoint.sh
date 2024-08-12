82380dca2c505172a086f0be97956875c65858d0
chantu <lynnleelhl@gmail.com>
2024-08-12 12:02:10 +0800

    build create ag sql

--- addons/mssql/scripts/entrypoint.sh
+++ addons/mssql/scripts/entrypoint.sh
@@ -4,47 +4,38 @@ function conn_local {
   echo "[DEBUG] $1"
   /opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P $MSSQL_SA_PASSWORD -C -Q "$1"
 }
-/opt/mssql/bin/mssql-conf set hadr.hadrenabled 1
-/opt/mssql/bin/sqlservr
-create_ag_sql=$(cat <<EOF
+function build_create_ag_sql {
+  create_ag_sql=$(cat <<EOF
 CREATE AVAILABILITY GROUP [ag1]
       WITH (DB_FAILOVER = ON, CLUSTER_TYPE = EXTERNAL)
       FOR REPLICA ON
-         N'<node1>'
-         WITH (
-            ENDPOINT_URL = N'tcp://<node1>:<5022>',
-            AVAILABILITY_MODE = SYNCHRONOUS_COMMIT,
-            FAILOVER_MODE = EXTERNAL,
-            SEEDING_MODE = AUTOMATIC
-            ),
-         N'<node2>'
-         WITH (
-            ENDPOINT_URL = N'tcp://<node2>:<5022>',
-            AVAILABILITY_MODE = SYNCHRONOUS_COMMIT,
-            FAILOVER_MODE = EXTERNAL,
-            SEEDING_MODE = AUTOMATIC
-            ),
-         N'<node3>'
-         WITH(
-            ENDPOINT_URL = N'tcp://<node3>:<5022>',
-            AVAILABILITY_MODE = SYNCHRONOUS_COMMIT,
-            FAILOVER_MODE = EXTERNAL,
-            SEEDING_MODE = AUTOMATIC
-            );
-ALTER AVAILABILITY GROUP [ag1] GRANT CREATE ANY DATABASE;
 EOF
-)
-KB_REPLICA_COUNT=3
-for i in {0..$KB_REPLICA_COUNT}; do
-  conf=$(cat <<EOF
-         N'$node'
+  )
+  KB_POD_LIST="mssql-mssql-0,mssql-mssql-1,mssql-mssql-2"
+  KB_CLUSTER_COMP_NAME="mssql-mssql"
+  IFS=',' read -ra pods <<< "$KB_POD_LIST"
+  for i in "${!pods[@]}"; do
+    pod_dns="${pods[$i]}.$KB_CLUSTER_COMP_NAME-headless"
+    conf=$(cat <<EOF
+         N'${pods[$i]}'
          WITH (
-              ENDPOINT_URL = N'tcp://$node:<5022>',
+              ENDPOINT_URL = N'tcp://$pod_dns:<5022>',
               AVAILABILITY_MODE = SYNCHRONOUS_COMMIT,
               FAILOVER_MODE = EXTERNAL,
               SEEDING_MODE = AUTOMATIC
               )
-  EOF
-  )
-  create_ag_sql="$create_ag_sql\n$conf"
-done
+EOF
+    )
+    create_ag_sql="$create_ag_sql\n$conf"
+    if [[ $i -eq $((${#pods[@]} - 1)) ]]; then
+      create_ag_sql="$create_ag_sql;"
+    else
+      create_ag_sql="$create_ag_sql,"
+    fi
+  done
+  create_ag_sql="$create_ag_sql\nALTER AVAILABILITY GROUP [ag1] GRANT CREATE ANY DATABASE;"
+}
+/opt/mssql/bin/mssql-conf set hadr.hadrenabled 1
+/opt/mssql/bin/sqlservr
+build_create_ag_sql
+echo $create_ag_sql
