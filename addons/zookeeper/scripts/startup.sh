d376d9e8648f2fba1f70c97b0c8e2886ff32b628
kubejocker <guobiaohan2020@163.com>
2024-08-21 12:58:33 +0800

    chore: replace extra-envs by componentSpecs.env

--- addons/zookeeper/scripts/startup.sh
+++ addons/zookeeper/scripts/startup.sh
@@ -9,8 +9,8 @@
 if [[ -f "/bitnami/zookeeper/data/myid" ]]; then
   export ZOO_SERVER_ID="$(cat /bitnami/zookeeper/data/myid)"
 else
-  SERVICE_ID=$((${KB_POD_NAME##*-}))
-  export ZOO_SERVER_ID="$((SERVICE_ID))"
+  SERVICE_ID=${CURRENT_POD_NAME##*-}
+  export ZOO_SERVER_ID=$SERVICE_ID
   echo $ZOO_SERVER_ID > /bitnami/zookeeper/data/myid
 fi
 
@@ -20,10 +20,10 @@ function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)"
 function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 
 
-if  version_gt "3.6.0" "${ZOOKEEPER_IMAGE_VERSION%%-*}"  ; then
-  scripts_path=""
-else
+if [ version_lt "3.6.0" "${ZOOKEEPER_IMAGE_VERSION%%-*}" ] || [ -z "${ZOOKEEPER_IMAGE_VERSION}" ]; then
   scripts_path="/opt/bitnami/scripts/zookeeper"
+else
+  scripts_path=""
 fi
 
 exec ${scripts_path}/entrypoint.sh ${scripts_path}/run.sh
