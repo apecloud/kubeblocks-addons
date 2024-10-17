ac993516f6f8f775a89df433f326adadc74a076e
xuriwuyun <xuriwuyun@gmail.com>
2024-10-17 17:12:40 +0800

    update entrypoints

--- addons/mysql/scripts/mysql-entrypoint.sh
+++ addons/mysql/scripts/mysql-entrypoint.sh
@@ -2,7 +2,7 @@
 REPORT_HOST=${KB_CLUSTER_COMP_NAME}-mysql-${SYNCER_POD_NAME##*-}
 SERVICE_ID=$((${SYNCER_POD_NAME##*-} + 1))
 if [ "${MYSQL_MAJOR}" = '5.7' ]; then
-  /scripts/docker-entrypoint-5.7.sh mysqld --server-id $SERVICE_ID --report-host ${REPORT_HOST} \
+  docker-entrypoint.sh mysqld --server-id $SERVICE_ID --report-host ${REPORT_HOST} \
     --ignore-db-dir=lost+found \
     --plugin-load-add=rpl_semi_sync_master=semisync_master.so \
     --plugin-load-add=rpl_semi_sync_slave=semisync_slave.so \
@@ -10,12 +10,12 @@ if [ "${MYSQL_MAJOR}" = '5.7' ]; then
     --log-bin=/var/lib/mysql/binlog/${SYNCER_POD_NAME}-bin \
     --skip-slave-start=$skip_slave_start
 elif [ "${MYSQL_MAJOR}" = '8.0' ]; then
-docker-entrypoint.sh mysqld --server-id $SERVICE_ID --report-host ${REPORT_HOST} \
-   --plugin-load-add=rpl_semi_sync_source=semisync_source.so \
-   --plugin-load-add=rpl_semi_sync_replica=semisync_replica.so \
-   --plugin-load-add=audit_log=audit_log.so \
-   --log-bin=/var/lib/mysql/binlog/${SYNCER_POD_NAME}-bin \
-   --skip-slave-start=$skip_slave_start
+  docker-entrypoint.sh mysqld --server-id $SERVICE_ID --report-host ${REPORT_HOST} \
+    --plugin-load-add=rpl_semi_sync_source=semisync_source.so \
+    --plugin-load-add=rpl_semi_sync_replica=semisync_replica.so \
+    --plugin-load-add=audit_log=audit_log.so \
+    --log-bin=/var/lib/mysql/binlog/${SYNCER_POD_NAME}-bin \
+    --skip-slave-start=$skip_slave_start
 else
   echo "Unsupported MySQL version"
   exit 1
