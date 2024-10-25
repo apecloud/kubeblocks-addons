81459e520c4d796faa3ce0260d3883132df5e1d4
Shun Ding <163502233+dingshun-cmss@users.noreply.github.com>
2024-10-25 09:31:00 +0800

    feat: logging for PostgreSQL wal-g scripts (#1120)

--- addons/postgresql/dataprotection/wal-g-delete.sh
+++ addons/postgresql/dataprotection/wal-g-delete.sh
@@ -1,27 +1,35 @@
+# shellcheck disable=SC2148
+
+postgres_log_dir="${VOLUME_DATA_DIR}/logs"
+postgres_scripts_log_file="${postgres_log_dir}/scripts.log"
+setup_logging WALG_DELETE "${postgres_scripts_log_file}"
+
 export WALG_DATASAFED_CONFIG=""
 export PATH="$PATH:$DP_DATASAFED_BIN_PATH"
 export DATASAFED_BACKEND_BASE_PATH="$DP_BACKUP_BASE_PATH"
 
 function getWalGSentinelInfo() {
-  local sentinelFile=${1}
-  local out=$(datasafed list ${sentinelFile})
+  local sentinelFile
+  local out
+  sentinelFile=${1}
+  out=$(datasafed list "${sentinelFile}")
   if [ "${out}" == "${sentinelFile}" ]; then
-     datasafed pull "${sentinelFile}" ${sentinelFile}
-     echo "$(cat ${sentinelFile})"
+     datasafed pull "${sentinelFile}" "${sentinelFile}"
+     cat "${sentinelFile}"
      return
   fi
 }
 
 # 1. get backup repo path of wal-g
 backupRepoPath=$(getWalGSentinelInfo "wal-g-backup-repo.path")
-if [[ -z ${backupRepoPath} ]]; then
+if [[ -z "${backupRepoPath}" ]]; then
    echo "INFO: nothing to delete."
    exit 0
 fi
 
 # 2. get backup name of this backup
 backupName=$(getWalGSentinelInfo "wal-g-backup-name")
-if [[ -z ${backupName} ]]; then
+if [[ -z "${backupName}" ]]; then
    echo "INFO: delete unsuccessfully backup files and outdated WAL archive."
    export DATASAFED_BACKEND_BASE_PATH=${backupRepoPath}
    wal-g delete garbage --confirm
@@ -29,16 +37,18 @@ if [[ -z ${backupName} ]]; then
 fi
 
 # 3. cleanup outdated wal logs, only effective when existing at least one full backup
-export DATASAFED_BACKEND_BASE_PATH=${backupRepoPath}
+echo "cleanup garbage archives in backup repo"
+export DATASAFED_BACKEND_BASE_PATH="${backupRepoPath}"
 wal-g delete garbage ARCHIVES
 
 # 4. delete wal-g
+echo "cleanup garbage full backups in backup repo"
 dpBackupFilesCount=$(datasafed list --name "${backupName}_dp_*" /basebackups_005 | wc -l)
-if [[ ${dpBackupFilesCount} -le 1 ]]; then
+if [[ "${dpBackupFilesCount}" -le 1 ]]; then
   # if this base backup only belongs to a backup CR, delete it.
-  echo "INFO: delete ${backupName}, backupRepo: ${backupRepo}"
-  wal-g delete target ${backupName} --confirm  && wal-g delete garbage ARCHIVES --confirm
+  echo "INFO: delete ${backupName}"
+  wal-g delete target "${backupName}" --confirm  && wal-g delete garbage ARCHIVES --confirm
 fi
 datasafed rm "/basebackups_005/${backupName}_dp_${DP_BACKUP_NAME}"
-
-
+echo "backup cleanup DONE"
+sync
