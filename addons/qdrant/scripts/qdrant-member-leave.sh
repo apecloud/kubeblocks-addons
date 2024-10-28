754b4338524c786a9a943b1fca5ab4acb1e7506d
Y-Rookie <yong.huang@apecloud.com>
2024-10-28 13:58:04 +0800

    add qdrant setup ut

--- addons/qdrant/scripts/qdrant-member-leave.sh
+++ addons/qdrant/scripts/qdrant-member-leave.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
 
-set -x
-set -o errtrace
-set -o nounset
-set -o pipefail
+# shellcheck disable=SC2034
+ut_mode="false"
+test || __() {
+  # when running in non-unit test mode, set the options "set -ex".
+  set -ex;
+}
 
-leave_peer_uri="http://${KB_LEAVE_MEMBER_POD_IP}:6333"
-cluster_info=$(curl -s "${leave_peer_uri}/cluster")
-leave_peer_id=$(echo "${cluster_info}" | jq -r .result.peer_id)
-leader_peer_id=$(echo "${cluster_info}" | jq -r .result.raft_info.leader)
+init_cluster_info() {
+  leave_peer_uri="http://${KB_LEAVE_MEMBER_POD_IP}:6333"
+  cluster_info=$(curl -s "${leave_peer_uri}/cluster")
+  leave_peer_id=$(echo "${cluster_info}" | jq -r .result.peer_id)
+  leader_peer_id=$(echo "${cluster_info}" | jq -r .result.raft_info.leader)
+}
 
 move_shards() {
   cols=$(curl -s "${leave_peer_uri}/collections")
@@ -37,15 +41,23 @@ move_shards() {
         "${leave_peer_uri}/collections/${col_name}/cluster"
     done
 
-    while true; do
-      col_cluster_info=$(curl -s "${leave_peer_uri}/collections/${col_name}/cluster")
-      leave_shard_ids=$(echo "${col_cluster_info}" | jq -r '.result.local_shards[].shard_id')
-      if [ -z "${leave_shard_ids}" ]; then
-        echo "all shards in collection ${col_name} has been moved"
-        break
-      fi
-      sleep 1
-    done
+    check_leave_shard_ids "${leave_peer_uri}" "${col_name}"
+  done
+}
+
+check_leave_shard_ids() {
+  leave_peer_uri=$1
+  col_name=$2
+
+  while true; do
+    col_cluster_info=$(curl -s "${leave_peer_uri}/collections/${col_name}/cluster")
+    leave_shard_ids=$(echo "${col_cluster_info}" | jq -r '.result.local_shards[].shard_id')
+    if [ -z "${leave_shard_ids}" ]; then
+      echo "all shards in collection ${col_name} has been moved"
+      break
+    fi
+    echo "shards ${leave_shard_ids} in collection ${col_name} are still moving..."
+    sleep 1
   done
 }
 
@@ -70,6 +82,7 @@ ${__SOURCED__:+false} : || return 0
 
 # lock file to prevent concurrent leave_member
 # flock will return 1 if the lock is already held by another process, this is expected
+init_cluster_info
 (
   if ! flock -n -x 9; then
     echo "member is already in leaving"
