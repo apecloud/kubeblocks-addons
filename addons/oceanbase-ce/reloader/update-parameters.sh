8aac4de0670a88c0ebb2acac38f3249efae85738
Shanshan <shanshan.ying@apecloud.com>
2024-11-13 10:56:58 +0800

    chore: update ob addon
    update cmpd with exporter
    
    update examples
    
    add hostnetwork example
    
    fix KB_ envs

--- addons/oceanbase-ce/reloader/update-parameters.sh
+++ addons/oceanbase-ce/reloader/update-parameters.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -ex
 
-OB_SERVICE_PORT=${COMP_MYSQL_PORT:-2881}
-OB_CLI="/kb_tools/obtools"
+CURRENT_PATH=$(cd $(dirname $0);pwd)
+source ${CURRENT_PATH}/utils.sh
 
 paramName="${1:?missing config}"
 paramValue="${2:?missing value}"
@@ -11,4 +11,8 @@ paramValue="${2:?missing value}"
 #  * MEMORY: specifies to modify only parameters in the memory, and the modification takes effect immediately. The modification becomes invalid after the server is restarted. However, no parameter supports this mode.
 #  * SPFILE: specifies to modify only parameters in the configuration table. The modification takes effect after the server is restarted.
 #  * BOTH: specifies to modify parameters in both the configuration table and the memory. The modification takes effect immediately and remains effective after the server is restarted.
-$OB_CLI --host 127.0.0.1 -uroot -P ${OB_SERVICE_PORT} param-update --set "${paramName}=${paramValue}" --scope BOTH
+obcli_cmd='$OB_CLI --host 127.0.0.1 -uroot -P ${OB_SERVICE_PORT} param-update --set "${paramName}=${paramValue}" --scope BOTH'
+if [ -n "${OB_ROOT_PASSWD}" ]; then
+  obcli_cmd="$obcli_cmd -p '${OB_ROOT_PASSWD}'"
+fi
+eval $obcli_cmd
