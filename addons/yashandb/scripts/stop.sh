b20a442a31b81c35a1e53e4612204ed46531c9ae
Y-Rookie <yong.huang@apecloud.com>
2024-11-14 19:04:41 +0800

    add init db ut

--- addons/yashandb/scripts/stop.sh
+++ addons/yashandb/scripts/stop.sh
@@ -2,7 +2,22 @@
 #
 # Stop YASDB database instance if running
 
-set -exuo pipefail
+# This is magic for shellspec ut framework. "test" is a `test [expression]` well known as a shell command.
+# Normally test without [expression] returns false. It means that __() { :; }
+# function is defined if this script runs directly.
+#
+# shellspec overrides the test command and returns true *once*. It means that
+# __() function defined internally by shellspec is called.
+#
+# In other words. If not in test mode, __ is just a comment. If test mode, __
+# is a interception point.
+# you should set ut_mode="true" when you want to run the script in shellspec file.
+#
+# shellcheck disable=SC2034
+ut_mode="false"
+test || __() {
+  set -exuo pipefail;
+}
 
 # Default configurations
 WORK_DIR=${WORK_DIR:-/home/yashan}
@@ -74,5 +89,12 @@ main() {
   load_environment
   stop_yasdb
 }
+
+# This is magic for shellspec ut framework.
+# Sometime, functions are defined in a single shell script.
+# You will want to test it. but you do not want to run the script.
+# When included from shellspec, __SOURCED__ variable defined and script
+# end here. The script path is assigned to the __SOURCED__ variable.
+${__SOURCED__:+false} : || return 0
 
 main "$@"
