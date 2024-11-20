d5f9711cdd584c8ba7a66353d71ac4f055850fe9
kubejocker <guobiaohan2020@163.com>
2024-11-20 09:58:09 +0800

    fix: zookeeper restore fail

--- addons/zookeeper/dataprotection/restore.sh
+++ addons/zookeeper/dataprotection/restore.sh
@@ -3,6 +3,7 @@ set -e
 set -o pipefail
 
 export PATH="$PATH:$DP_DATASAFED_BIN_PATH"
+export DATASAFED_BACKEND_BASE_PATH="$DP_BACKUP_BASE_PATH"
 TMP_DIR=/tmp/zookeeper/restore
 mkdir -p ${TMP_DIR}
 
