03a8c5e43654d68a2e560fdc637c35bb0b2faea5
Y-Rookie <yong.huang@apecloud.com>
2024-12-10 17:31:39 +0800

    upgrade orielodb addon to v1.0 API

--- addons/orioledb/scripts/generate_patroni_yaml.py
+++ addons/orioledb/scripts/generate_patroni_yaml.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import os
 import sys
 import yaml
@@ -41,56 +40,28 @@ def postgresql_conf_to_dict(file_path):
 def main(filename):
     restore_dir = os.environ.get('RESTORE_DATA_DIR', '')
     local_config = yaml.safe_load(
-        os.environ.get('SPILO_CONFIGURATION', os.environ.get('PATRONI_CONFIGURATION', ''))) or {}
-    pod_ip = os.environ.get('POD_IP')
-    # scope
-    local_config['scope'] = os.environ.get('SCOPE')
-    # name
-    local_config['name'] = os.environ.get('CURRENT_POD_NAME')
-    # etcd3
-    local_config['etcd3'] = {
-        'host': os.environ.get('PATRONI_DCS_ETCD_SERVICE_ENDPOINT')
-    }
-    # restapi
-    local_config['restapi'] = {
-        'listen': f'{pod_ip}:8008',
-        'connect_address': f'{pod_ip}:8008',
-    }
-    # postgresql
+        os.environ.get('SPILO_CONFIGURATION',
+                       os.environ.get('PATRONI_CONFIGURATION', ''))) or {}
+
+    # set postgresql parameters
     if 'postgresql' not in local_config:
         local_config['postgresql'] = {}
     postgresql = local_config['postgresql']
-    postgresql['data_dir'] = os.environ.get('PGDATA')
     postgresql['config_dir'] = '/home/postgres/pgdata/conf'
     postgresql['custom_conf'] = '/home/postgres/conf/postgresql.conf'
-    postgresql['authentication'] = {}
-    postgresql['listen'] = '0.0.0.0:5432'
-    postgresql['connect_address'] = f'{pod_ip}:5432'
-    authentication = postgresql['authentication']
-    authentication['superuser'] = {"username": os.environ.get('PGUSER_SUPERUSER'),
-                                   'password': os.environ.get('PGPASSWORD_SUPERUSER')}
-    authentication['replication'] = {'username': os.environ.get('PGUSER_SUPERUSER'),
-                                     'password': os.environ.get('POSTGRES_PASSWORD')}
 
     # add pg_hba.conf
     with open('/home/postgres/conf/pg_hba.conf', 'r') as f:
         lines = read_file_lines(f)
         if lines:
             postgresql['pg_hba'] = lines
-
-    if restore_dir and os.path.isfile(os.path.join(restore_dir, 'kb_restore.signal')):
+    if restore_dir and os.path.isfile(
+            os.path.join(restore_dir, 'kb_restore.signal')):
         if 'bootstrap' not in local_config:
             local_config['bootstrap'] = {}
         with open('/home/postgres/conf/kb_restore.conf', 'r') as f:
             local_config['bootstrap'].update(yaml.safe_load(f))
 
-    local_config['bootstrap'] = {}
-    bootstrap = local_config['bootstrap']
-    bootstrap['dcs'] = {
-        'postgresql': {
-            'parameters': {'listen_addresses': "0.0.0.0", "port": 5432}
-        }
-    }
     # point in time recovery(PITR)
     if os.path.isfile("/home/postgres/pgdata/conf/recovery.conf"):
         with open('/home/postgres/conf/kb_pitr.conf', 'r') as f:
@@ -98,7 +69,6 @@ def main(filename):
             re_config = postgresql_conf_to_dict("/home/postgres/pgdata/conf/recovery.conf")
             pitr_config[pitr_config['method']]['recovery_conf'].update(re_config)
             local_config['bootstrap'].update(pitr_config)
-
     # patroni parameters
     if 'bootstrap' not in local_config:
         local_config['bootstrap'] = {}
@@ -109,8 +79,6 @@ def main(filename):
             local_config['bootstrap']['dcs'].update(yaml.safe_load(f))
     else:
         print('patroni.yaml not found')
-
-    # bootstrap
     write_file(yaml.dump(local_config, default_flow_style=False), filename, True)
 
 
