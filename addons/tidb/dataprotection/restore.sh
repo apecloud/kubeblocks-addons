e45f5b4a510ab1c1d10e3dbd9f81475c0cf8c9fa
Harold Cheng <niuchangcun@gmail.com>
2024-12-24 16:29:48 +0800

    fix: grep bucket error when access_key_id's name is "root"

--- addons/tidb/dataprotection/restore.sh
+++ addons/tidb/dataprotection/restore.sh
@@ -13,7 +13,8 @@ function getToolConfigValue() {
 access_key_id=$(getToolConfigValue access_key_id)
 secret_access_key=$(getToolConfigValue secret_access_key)
 endpoint=$(getToolConfigValue endpoint)
-bucket=$(getToolConfigValue root)
+# workaround for "access_key_id = root" 
+bucket=$(getToolConfigValue "root =")
 
 # FIXME: hardcoded port
 /br restore full --pd "$DP_DB_HOST:2379" --storage "s3://$bucket$DP_BACKUP_BASE_PATH?access-key=$access_key_id&secret-access-key=$secret_access_key" --s3.endpoint "$endpoint"
