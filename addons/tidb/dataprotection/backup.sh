a2c7f8ce43f38885a9c17143f1a91053e841bbd6
Harold Cheng <niuchangcun@gmail.com>
2025-01-07 16:31:11 +0800

    remove workaround

--- addons/tidb/dataprotection/backup.sh
+++ addons/tidb/dataprotection/backup.sh
@@ -19,14 +19,13 @@ trap handle_exit EXIT
 
 function getToolConfigValue() {
     local var=$1
-    cat $toolConfig | grep "$var" | awk '{print $NF}'
+    cat $toolConfig | grep "^$var" | awk '{print $NF}'
 }
 
 access_key_id=$(getToolConfigValue access_key_id)
 secret_access_key=$(getToolConfigValue secret_access_key)
 endpoint=$(getToolConfigValue endpoint)
-# workaround for "access_key_id = root" 
-bucket=$(getToolConfigValue "root =")
+bucket=$(getToolConfigValue "root")
 
 # FIXME: hardcoded port
 /br backup full --pd "$DP_DB_HOST:2379" --storage "s3://$bucket$DP_BACKUP_BASE_PATH?access-key=$access_key_id&secret-access-key=$secret_access_key" --s3.endpoint "$endpoint"
