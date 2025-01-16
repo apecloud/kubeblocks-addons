3831f806afb25cc73dc897debe879bd0c2159e22
Shanshan <shanshan.ying@apecloud.com>
2025-01-16 19:35:37 +0800

    chore: add kb-addon-reviewers group (#1412)

--- hack/update-codeowners.sh
+++ hack/update-codeowners.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FALLBACKS="@leon-inf @Y-Rookie @apecloud/kb-reviewers"
+FALLBACKS="@leon-inf @Y-Rookie @apecloud/kb-reviewers @apecloud/kb-addon-reviewers"
 
 function codeowners_content() {
   cat <<-EOF
