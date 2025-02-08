f50d4adad7205c4c4d3abe5b117dd769d2deab28
wangyelei <yelei.wang@apecloud.com>
2025-02-08 11:06:27 +0800

    chore: support to continuous and incremental backup with the wal-g tool (#1456)
    (cherry picked from commit d86da8ed88aa165d1e90d8091500325bdb4e536a)

--- addons/postgresql/dataprotection/wal-g-archive-restore.sh
+++ addons/postgresql/dataprotection/wal-g-archive-restore.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 set -e
-export WALG_DATASAFED_CONFIG=""
-export WALG_COMPRESSION_METHOD=zstd
-export PATH="$PATH:$DP_DATASAFED_BIN_PATH"
-# 20Gi for bundle file
-export WALG_TAR_SIZE_THRESHOLD=21474836480
-export DATASAFED_BACKEND_BASE_PATH="$DP_BACKUP_BASE_PATH"
-
-function getWalGSentinelInfo() {
-  local sentinelFile=${1}
-  local out=$(datasafed list ${sentinelFile})
-  if [ "${out}" == "${sentinelFile}" ]; then
-     datasafed pull "${sentinelFile}" ${sentinelFile}
-     echo "$(cat ${sentinelFile})"
-     return
-  fi
-}
+backup_base_path="$(dirname $DP_BACKUP_BASE_PATH)/wal-g/"
 
 function config_wal_g_for_fetch_wal_log() {
     walg_dir=${VOLUME_DATA_DIR}/wal-g
@@ -32,20 +17,7 @@ function config_wal_g_for_fetch_wal_log() {
     echo "zstd" > ${walg_env}/WALG_COMPRESSION_METHOD
 }
 
-# 1. get restore info
-backupRepoPath=$(getWalGSentinelInfo "wal-g-backup-repo.path")
-backupName=$(getWalGSentinelInfo "wal-g-backup-name")
-
-# 2. fetch base backup
-export DATASAFED_BACKEND_BASE_PATH="${backupRepoPath}"
-mkdir -p ${DATA_DIR};
-wal-g backup-fetch ${DATA_DIR} ${backupName}
-
-# if restore timestamp is specified, skip restore wal logs
-if [ ! -z "${DP_RESTORE_TIMESTAMP}" ]; then
-   exit 0
-fi
-# 3. config restore script
+# 1. config restore script
 touch ${DATA_DIR}/recovery.signal;
 mkdir -p ${RESTORE_SCRIPT_DIR} && chmod 777 -R ${RESTORE_SCRIPT_DIR};
 echo "#!/bin/bash" > ${RESTORE_SCRIPT_DIR}/kb_restore.sh;
@@ -53,18 +25,19 @@ echo "[[ -d '${DATA_DIR}.old' ]] && mv -f ${DATA_DIR}.old/* ${DATA_DIR}/;" >> ${
 echo "sync;" >> ${RESTORE_SCRIPT_DIR}/kb_restore.sh;
 chmod +x ${RESTORE_SCRIPT_DIR}/kb_restore.sh;
 
-# 4. config wal-g to fetch wal logs
-config_wal_g_for_fetch_wal_log "${backupRepoPath}"
+# 2. config wal-g to fetch wal logs
+config_wal_g_for_fetch_wal_log "${backup_base_path}"
 
-# 5. config restore command
+# 3. config restore command
 mkdir -p ${CONF_DIR} && chmod 777 -R ${CONF_DIR};
 WALG_DIR=/home/postgres/pgdata/wal-g
 
 restore_command_str="envdir ${WALG_DIR}/restore-env ${WALG_DIR}/wal-g wal-fetch %f %p >> ${RESTORE_SCRIPT_DIR}/wal-g.log 2>&1"
 cat << EOF > "${CONF_DIR}/recovery.conf"
 restore_command='${restore_command_str}'
-recovery_target='immediate'
+recovery_target_time='$( date -d "@${DP_RESTORE_TIMESTAMP}" '+%F %T%::z' )'
 recovery_target_action='promote'
+recovery_target_timeline='latest'
 EOF
 # this step is necessary, data dir must be empty for patroni
 mv ${DATA_DIR} ${DATA_DIR}.old
