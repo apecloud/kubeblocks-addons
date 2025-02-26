ed4df5c715847a7882605df831406da4c6eede4f
wangyelei <yelei.wang@apecloud.com>
2025-02-26 15:45:44 +0800

    fix: improve wal log deletion policy for wal-g (#1498)
    (cherry picked from commit fc75b043c6db32a7d3b64a1305c54100cd4c4c69)

--- addons/postgresql/dataprotection/wal-g-archive-delete.sh
+++ addons/postgresql/dataprotection/wal-g-archive-delete.sh
@@ -4,6 +4,9 @@ export WALG_DATASAFED_CONFIG=""
 export PATH="$PATH:$DP_DATASAFED_BIN_PATH"
 export DATASAFED_BACKEND_BASE_PATH="$backup_base_path"
 
+# cleanup outdated wal logs, only effective when existing at least one full backup
+wal-g delete garbage ARCHIVES --confirm
+
 base_backup_list=$(datasafed list /basebackups_005 -d)
 if [[ -z ${base_backup_list} ]]; then
   datasafed rm -r /wal_005
