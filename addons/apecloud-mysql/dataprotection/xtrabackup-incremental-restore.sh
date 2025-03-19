82735275bc09426c45483d49b675d84d747eaa92
wanglong <46259301+gnolong@users.noreply.github.com>
2025-03-19 15:35:34 +0800

    fix: align target instance of mysql inc backup (#1534)
    (cherry picked from commit 1a315f8d35bd87924b67aa7c8e480421cb6482f5)

--- addons/apecloud-mysql/dataprotection/xtrabackup-incremental-restore.sh
+++ addons/apecloud-mysql/dataprotection/xtrabackup-incremental-restore.sh
@@ -15,6 +15,7 @@ function download_backup_file() {
   local_target_dir=$2
   mkdir -p ${local_target_dir} && cd ${local_target_dir}
   change_backend_path "${backup_name}"
+  echo "Downloading backup file ${backup_name}.xbstream"
   datasafed pull "${backup_name}.xbstream" - | xbstream -x
   xtrabackup --decompress --remove-original --target-dir=${local_target_dir}
 }
@@ -25,37 +26,37 @@ if [[ -z ${DP_BASE_BACKUP_NAME} ]]; then
   exit 1
 fi
 
-# 2. download backup files
-# download base backup file
+# 2. prepare backup files
+# prepare base data
 mkdir -p ${DATA_DIR}
 BASE_DIR=${DATA_MOUNT_DIR}/xtrabackup-base
 download_backup_file "${DP_BASE_BACKUP_NAME}" "${BASE_DIR}"
-# download parent backup files
+xtrabackup --prepare --apply-log-only --target-dir=${BASE_DIR}
+
+# get ancestor incremental backup names
 if [ -n "${DP_ANCESTOR_INCREMENTAL_BACKUP_NAMES}" ]; then
   read -r -a ANCESTOR_INCREMENTAL_BACKUP_NAMES <<< "${DP_ANCESTOR_INCREMENTAL_BACKUP_NAMES//,/ }"
 fi
 INCS_DIR=${DATA_MOUNT_DIR}/xtrabackup-incs
 mkdir -p ${INCS_DIR}
+# prepare incremental data
 for parent_name in "${ANCESTOR_INCREMENTAL_BACKUP_NAMES[@]}"; do
   download_backup_file "${parent_name}" "${INCS_DIR}/${parent_name}"
+  xtrabackup --prepare --apply-log-only --target-dir=${BASE_DIR} --incremental-dir=${INCS_DIR}/${parent_name}
+  rm -rf ${INCS_DIR}/${parent_name}
 done
-# download target backup file
+
+# prepare the last data
 download_backup_file "${DP_BACKUP_NAME}" "${INCS_DIR}/${DP_BACKUP_NAME}"
+xtrabackup --prepare --target-dir=${BASE_DIR} --incremental-dir=${INCS_DIR}/${DP_BACKUP_NAME}
+rm -rf ${INCS_DIR}/${DP_BACKUP_NAME}
 
 old_signal="apecloud-mysql.old"
 log_bin=${LOG_BIN}
 if [ "$(datasafed list ${old_signal})" == "${old_signal}" ]; then
    log_bin="${DATA_DIR}/mysql-bin"
 fi
-
-# 3. prepare data
-xtrabackup --prepare --apply-log-only --target-dir=${BASE_DIR}
-for parent_name in "${ANCESTOR_INCREMENTAL_BACKUP_NAMES[@]}"; do
-  xtrabackup --prepare --apply-log-only --target-dir=${BASE_DIR} --incremental-dir=${INCS_DIR}/${parent_name}
-done
-xtrabackup --prepare --target-dir=${BASE_DIR} --incremental-dir=${INCS_DIR}/${DP_BACKUP_NAME}
-
-# 4. restore
+# 3. restore
 xtrabackup --move-back --target-dir=${BASE_DIR} --datadir=${DATA_DIR}/ --log-bin=${log_bin}
 touch ${DATA_DIR}/${SIGNAL_FILE}
 rm -rf ${BASE_DIR}
