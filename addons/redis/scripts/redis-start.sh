727ca9118da8edbb66c43800c0ea5169120406f3
xingran <warnroot007@gmail.com>
2025-04-18 16:41:11 +0800

    chore: refine redis host network vars (#1603)

--- addons/redis/scripts/redis-start.sh
+++ addons/redis/scripts/redis-start.sh
@@ -330,7 +330,7 @@ parse_redis_announce_addr() {
   if [[ -z "${REDIS_ADVERTISED_PORT}" ]]; then
     echo "Environment variable REDIS_ADVERTISED_PORT not found. Ignoring."
     # if redis is in host network mode, use the host ip and port as the announce ip and port
-    if [[ -n "${REDIS_HOST_NETWORK_PORT}" ]] && [[ -n "$HOST_NETWORK_ENABLED" ]]; then
+    if [[ -n "${REDIS_HOST_NETWORK_PORT}" ]]; then
       echo "redis is in host network mode, use the host ip:$KB_HOST_IP and port:$REDIS_HOST_NETWORK_PORT as the announce ip and port."
       redis_announce_port_value="$REDIS_HOST_NETWORK_PORT"
       redis_announce_host_value="$KB_HOST_IP"
