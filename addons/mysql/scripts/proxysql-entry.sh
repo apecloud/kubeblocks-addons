eaf0156a60084011f12d87a57e4954c96f29f6a2
Shanshan <shanshan.ying@apecloud.com>
2025-04-21 17:44:39 +0800

    chore: update mysql proxy tpl

--- addons/mysql/scripts/proxysql-entry.sh
+++ addons/mysql/scripts/proxysql-entry.sh
@@ -20,27 +20,6 @@ if [ $FRONTEND_TLS_ENABLED == "true" ]; then
     cp /var/lib/frontend/server/tls.key /var/lib/proxysql/proxysql-key.pem
 fi
 
-function gen_mysql_servers {
-  IFS=',' read -r -a MYSQL_FQDNS_ARRAY <<< "$MYSQL_FQDNS"
-  result=""
-
-  for fqdn in "${MYSQL_FQDNS_ARRAY[@]}"; do
-    index=$(echo "$fqdn" | grep -oP '\d+(?=\.)')
-    HOSTGROUP_ID=3
-    if [ "$index" -eq 0 ]; then
-      HOSTGROUP_ID=2
-    fi
-    config="  { hostgroup_id = $HOSTGROUP_ID , hostname = \"$fqdn\", port = 3306, weight = 1, use_ssl = 0 }"
-    if [ -z "$result" ]; then
-      result="$config"
-    else
-      result="$result, \n$config"
-    fi
-  done
-
-  echo "$result"
-}
-
 # If command has arguments, prepend proxysql
 if [ "${1:0:1}" = '-' ]; then
     CMDARG="$@"
@@ -52,9 +31,7 @@ if [ "${__SOURCED__:+x}" ]; then
 fi
 
 function replace_config_variables() {
-  mysql_servers=$(gen_mysql_servers)
-  awk -v mysql_servers="$mysql_servers" '{gsub(/\${MYSQL_SERVERS}/, mysql_servers); print}' /config/custom-config/proxysql.tpl > /proxysql.cnf
-
+  cat /config/custom-config/proxysql.tpl > /proxysql.cnf
   sed -i "s|\${PROXYSQL_MONITOR_PASSWORD}|${PROXYSQL_MONITOR_PASSWORD}|g" /proxysql.cnf
   sed -i "s|\${PROXYSQL_CLUSTER_PASSWORD}|${PROXYSQL_CLUSTER_PASSWORD}|g" /proxysql.cnf
   sed -i "s|\${PROXYSQL_ADMIN_PASSWORD}|${PROXYSQL_ADMIN_PASSWORD}|g" /proxysql.cnf
