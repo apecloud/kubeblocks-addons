fe6b938f17efe198a4bbaa7e0a260cc7a5a43e21
gnolong <2391353625@qq.com>
2025-06-11 15:22:30 +0800

    fix

--- addons/mongodb/dataprotection/common-scripts.sh
+++ addons/mongodb/dataprotection/common-scripts.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # if the script exits with a non-zero exit code, touch a file to indicate that the backup failed,
 # the sync progress container will check this file and exit if it exists
 function handle_exit() {
