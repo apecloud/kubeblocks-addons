99fcb403ad86c36b3a6966ddde4bbc0d2d8a5f46
kizuna-lek <asd98041@qq.com>
2025-07-07 11:46:28 +0800

    chore: improve zookeeper jvm settings in 0.9

--- addons/zookeeper/scripts/startup.sh
+++ addons/zookeeper/scripts/startup.sh
@@ -16,6 +16,32 @@ function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" 
 function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
 function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" == "$1"; }
 
+function set_jvm_configuration() {
+  system_memory_in_mb=$(($(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)/1024/1024))
+  # set max heap size based on the following
+  # max(min(1/2 ram, 1024MB), min(1/4 ram, 8GB))
+  half_system_memory_in_mb=$((system_memory_in_mb / 2))
+  quarter_system_memory_in_mb=$((half_system_memory_in_mb / 2))
+  if [ "$half_system_memory_in_mb" -gt "1024" ]; then
+      half_system_memory_in_mb="1024"
+  fi
+  if [ "$quarter_system_memory_in_mb" -gt "8192" ]; then
+      quarter_system_memory_in_mb="8192"
+  fi
+  if [ "$half_system_memory_in_mb" -gt "$quarter_system_memory_in_mb" ]; then
+      max_heap_size_in_mb="$half_system_memory_in_mb"
+  else
+      max_heap_size_in_mb="$quarter_system_memory_in_mb"
+  fi
+  MAX_HEAP_SIZE="${max_heap_size_in_mb}M"
+
+  export JVMFLAGS="$JVMFLAGS \
+        -XX:+UseG1GC \
+        -Xlog:gc:/opt/bitnami/zookeeper/logs/gc.log
+        -Xlog:gc* \
+        -XX:NewRatio=2 \
+        -Xms$MAX_HEAP_SIZE -Xmx$MAX_HEAP_SIZE"
+}
 
 if [ -z "${ZOOKEEPER_IMAGE_VERSION}" ] ||  version_lt "3.6.0" "${ZOOKEEPER_IMAGE_VERSION%%-*}"  ; then
   scripts_path="/opt/bitnami/scripts/zookeeper"
@@ -23,4 +49,5 @@ else
   scripts_path=""
 fi
 
+set_jvm_configuration
 exec ${scripts_path}/entrypoint.sh ${scripts_path}/run.sh
