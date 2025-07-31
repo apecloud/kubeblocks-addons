6aaade9375017fb0ed093f84ca8cbfcb19f2a94d
Harold Cheng <niuchangcun@gmail.com>
2025-07-31 16:27:41 +0800

    remove sensitive info in backup log

--- addons/tidb/dataprotection/backup.sh
+++ addons/tidb/dataprotection/backup.sh
@@ -14,6 +14,7 @@ function handle_exit() {
 trap handle_exit EXIT
 setStorageVar
 
+echo "start full backup"
 # shellcheck disable=SC2086
 /br backup full --pd "$PD_ADDRESS" --storage "s3://$BUCKET$DP_BACKUP_BASE_PATH?access-key=$ACCESS_KEY_ID&secret-access-key=$SECRET_ACCESS_KEY" --s3.endpoint "$ENDPOINT" $BR_EXTRA_ARGS
 
