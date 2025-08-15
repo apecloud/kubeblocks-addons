3250642312810ca3b12a7de6c9ad268e76b4ef8f
wangyelei <yelei.wang@apecloud.com>
2025-08-15 15:47:37 +0800

    chore: add config-replica-wal-g backup method for pg

--- addons/postgresql/dataprotection/wal-g-config-restore.sh
+++ addons/postgresql/dataprotection/wal-g-config-restore.sh
@@ -1,14 +1,9 @@
-set -e
+#!/bin/bash
 
-if [[ ! -f /etc/datasafed/datasafed.conf ]]; then
-  echo "ERROR: backupRepo should use Tool accessMode"
-  exit 1
-fi
-
-function config_wal_g() {
+function config_wal_g_for_fetch_wal_log() {
     walg_dir=${VOLUME_DATA_DIR}/wal-g
-    walg_env=${walg_dir}/env
-    mkdir -p ${walg_dir}/env
+    walg_env=${walg_dir}/restore-env
+    mkdir -p ${walg_dir}/restore-env
     cp /etc/datasafed/datasafed.conf ${walg_dir}/datasafed.conf
     cp /usr/bin/wal-g ${walg_dir}/wal-g
     datasafed_base_path=${1:?missing datasafed_base_path}
@@ -17,7 +12,6 @@ function config_wal_g() {
     # echo "" > ${walg_env}/WALG_PG_WAL_SIZE
     echo "${walg_dir}/datasafed.conf" > ${walg_env}/WALG_DATASAFED_CONFIG
     echo "${datasafed_base_path}" > ${walg_env}/DATASAFED_BACKEND_BASE_PATH
-    echo "true" > ${walg_env}/PG_READY_RENAME
     echo "zstd" > ${walg_env}/WALG_COMPRESSION_METHOD
     if [ -n "${DATASAFED_ENCRYPTION_ALGORITHM}" ]; then
       echo "${DATASAFED_ENCRYPTION_ALGORITHM}" > ${walg_env}/DATASAFED_ENCRYPTION_ALGORITHM
@@ -31,6 +25,4 @@ function config_wal_g() {
     fi
 }
 
-config_wal_g "$(dirname $(dirname $DP_BACKUP_BASE_PATH))/wal-g"
-echo "{\"timeRange\":{\"end\":\"2023-05-06T00:00:00Z\"}}" >"${DP_BACKUP_INFO_FILE}"
-sync
+config_wal_g_for_fetch_wal_log "$(dirname $(dirname $DP_BACKUP_BASE_PATH))/wal-g"
