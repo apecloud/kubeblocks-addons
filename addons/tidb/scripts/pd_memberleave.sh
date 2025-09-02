f0a45beaa5c053a751f7b32fe789e167e7852e53
cjc7373 <niuchangcun@gmail.com>
2025-09-02 11:18:25 +0800

    fix: tidb minor problems (#1943)
    (cherry picked from commit 80b62e6c818a59a9671b973922c0157cc5328404)

--- addons/tidb/scripts/pd_memberleave.sh
+++ addons/tidb/scripts/pd_memberleave.sh
@@ -2,10 +2,8 @@
 
 set -exo pipefail
 
-ADDRESS=${KB_MEMBER_ADDRESSES%%,*}
 echo "$KB_LEAVE_MEMBER_POD_NAME"
-echo "$ADDRESS"
-res=$(/pd-ctl -u "$ADDRESS" member delete name "$KB_LEAVE_MEMBER_POD_NAME")
+res=$(/pd-ctl member delete name "$KB_LEAVE_MEMBER_POD_NAME")
 echo "$res"
 not_found_pattern="Failed to delete member.*not found"
 if [[ $res != "Success!" && ! $res =~ $not_found_pattern ]]; then
