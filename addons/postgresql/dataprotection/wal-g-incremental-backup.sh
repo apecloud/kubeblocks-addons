29a979649e319e400c1cc3791650aef4035cdaa9
Dongming <l.dongming@apecloud.com>
2025-09-03 11:05:21 +0800

    chore: pg remove config-wal-g action set (#1949)
    Co-authored-by: wangyelei <yelei.wang@apecloud.com>
    Co-authored-by: ldming <l.dongming@apecloud.com>

--- addons/postgresql/dataprotection/wal-g-incremental-backup.sh
+++ addons/postgresql/dataprotection/wal-g-incremental-backup.sh
@@ -22,6 +22,32 @@ function handle_exit() {
   fi
 }
 
+function config_wal_g() {
+    walg_dir=${VOLUME_DATA_DIR}/wal-g
+    walg_env=${walg_dir}/env
+    mkdir -p ${walg_dir}/env
+    cp /etc/datasafed/datasafed.conf ${walg_dir}/datasafed.conf
+    cp /usr/bin/wal-g ${walg_dir}/wal-g
+    datasafed_base_path=${1:?missing datasafed_base_path}
+    # config wal-g env
+    # config WALG_PG_WAL_SIZE with wal_segment_size which fetched by psql
+    # echo "" > ${walg_env}/WALG_PG_WAL_SIZE
+    echo "${walg_dir}/datasafed.conf" > ${walg_env}/WALG_DATASAFED_CONFIG
+    echo "${datasafed_base_path}" > ${walg_env}/DATASAFED_BACKEND_BASE_PATH
+    echo "true" > ${walg_env}/PG_READY_RENAME
+    echo "zstd" > ${walg_env}/WALG_COMPRESSION_METHOD
+    if [ -n "${DATASAFED_ENCRYPTION_ALGORITHM}" ]; then
+      echo "${DATASAFED_ENCRYPTION_ALGORITHM}" > ${walg_env}/DATASAFED_ENCRYPTION_ALGORITHM
+    elif [ -f ${walg_env}/DATASAFED_ENCRYPTION_ALGORITHM ]; then
+       rm ${walg_env}/DATASAFED_ENCRYPTION_ALGORITHM
+    fi
+    if [ -n "${DATASAFED_ENCRYPTION_PASS_PHRASE}" ]; then
+       echo "${DATASAFED_ENCRYPTION_PASS_PHRASE}" > ${walg_env}/DATASAFED_ENCRYPTION_PASS_PHRASE
+    elif [ -f ${walg_env}/DATASAFED_ENCRYPTION_PASS_PHRASE ]; then
+       rm ${walg_env}/DATASAFED_ENCRYPTION_PASS_PHRASE
+    fi
+}
+
 function getWalGSentinelInfo() {
   local sentinelFile=${1}
   local out=$(datasafed list ${sentinelFile})
@@ -61,6 +87,9 @@ if [[ -z ${DP_PARENT_BACKUP_NAME} ]]; then
   echo "DP_PARENT_BACKUP_NAME is empty"
   exit 1
 fi
+
+# config wal-g
+config_wal_g "$(dirname $DP_BACKUP_BASE_PATH)/wal-g"
 
 # 2. parent backup name of the wal-g
 export DATASAFED_BACKEND_BASE_PATH=$(dirname ${DP_BACKUP_BASE_PATH})/${DP_PARENT_BACKUP_NAME}
