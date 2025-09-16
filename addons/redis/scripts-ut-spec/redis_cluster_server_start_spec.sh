68a590bed71ce5417a90c7c93ec2bd4783bf30b3
loomt <loomt_@outlook.com>
2025-09-16 11:39:42 +0800

    revert ut

--- addons/redis/scripts-ut-spec/redis_cluster_server_start_spec.sh
+++ addons/redis/scripts-ut-spec/redis_cluster_server_start_spec.sh
@@ -305,10 +305,9 @@ Describe "Redis Cluster Server Start Bash Script Tests"
         export CURRENT_SHARD_ADVERTISED_PORT="redis-shard-sxj-0:31000,redis-shard-sxj-1:31001"
         export CURRENT_SHARD_ADVERTISED_BUS_PORT="redis-shard-sxj-0:32000,redis-shard-sxj-1:32001"
         export CURRENT_SHARD_COMPONENT_NAME="redis-shard-sxj"
-        current_comp_primary_node=()
-        current_comp_other_nodes=()
-        other_comp_primary_nodes=()
-        other_comp_other_nodes=()
+        export current_comp_primary_node=()
+        export current_comp_other_nodes=()
+        export other_comp_primary_nodes=()
       }
       Before "setup"
 
@@ -322,8 +321,9 @@ Describe "Redis Cluster Server Start Bash Script Tests"
       It "parses current component nodes correctly when using advertised ports"
         When call get_current_comp_nodes_for_scale_out_replica "redis-shard-sxj-0.redis-shard-sxj-headless.default.svc" "6379"
         The status should be success
-        The variable current_comp_primary_node[0] should equal "10.42.0.227#redis-shard-sxj-0.redis-shard-sxj-headless.default.svc#10.42.0.227:31000@32000"
-        The variable current_comp_other_nodes[0] should equal "10.42.0.228#redis-shard-sxj-1.redis-shard-sxj-headless.default.svc#10.42.0.228:31001@32001"
+        The variable current_comp_primary_node should equal "10.42.0.227#redis-shard-sxj-0.redis-shard-sxj-headless.default.svc#10.42.0.227:31000@32000"
+        The variable current_comp_other_nodes should equal "10.42.0.228#redis-shard-sxj-1.redis-shard-sxj-headless.default.svc#10.42.0.228:31001@32001"
+        The variable other_comp_primary_nodes should equal "10.42.0.229#redis-shard-abc-0.redis-shard-abc-headless.default.svc#10.42.0.229:32222@32223"
         The stdout should include "other_comp_other_nodes: "
       End
     End
@@ -338,10 +338,10 @@ Describe "Redis Cluster Server Start Bash Script Tests"
         unset CURRENT_SHARD_ADVERTISED_BUS_PORT
         export CURRENT_SHARD_COMPONENT_NAME="redis-shard-sxj"
         export SERVICE_PORT="6379"
-        current_comp_primary_node=()
-        current_comp_other_nodes=()
-        other_comp_primary_nodes=()
-        other_comp_other_nodes=()
+        export current_comp_primary_node=()
+        export current_comp_other_nodes=()
+        export other_comp_primary_nodes=()
+        export other_comp_other_nodes=()
       }
       Before "setup"
 
@@ -354,10 +354,9 @@ Describe "Redis Cluster Server Start Bash Script Tests"
 
       It "parses current component nodes correctly when not using advertised ports"
         When call get_current_comp_nodes_for_scale_out_replica "redis-shard-sxj-0.redis-shard-sxj-headless.default.svc" "6379"
-        The status should be success
-        The variable current_comp_primary_node[0] should equal "10.42.0.227#redis-shard-sxj-0.redis-shard-sxj-headless.default.svc#redis-shard-sxj-0.redis-shard-sxj-headless.default.svc:6379@16379"
-        The variable current_comp_other_nodes[0] should equal "10.42.0.228#redis-shard-sxj-1.redis-shard-sxj-headless.default.svc#redis-shard-sxj-1.redis-shard-sxj-headless.default.svc:6379@16379"
-        The variable other_comp_primary_nodes[0] should equal "10.42.0.229#redis-shard-abc-0.redis-shard-abc-headless.default.svc#redis-shard-abc-0.redis-shard-abc-headless.default.svc:6379@16379"
+        The variable current_comp_primary_node should equal "10.42.0.227#redis-shard-sxj-0.redis-shard-sxj-headless.default.svc#redis-shard-sxj-0.redis-shard-sxj-headless.default.svc:6379@16379"
+        The variable current_comp_other_nodes should equal "10.42.0.228#redis-shard-sxj-1.redis-shard-sxj-headless.default.svc#redis-shard-sxj-1.redis-shard-sxj-headless.default.svc:6379@16379"
+        The variable other_comp_primary_nodes should equal "10.42.0.229#redis-shard-abc-0.redis-shard-abc-headless.default.svc#redis-shard-abc-0.redis-shard-abc-headless.default.svc:6379@16379"
         The stdout should include "other_comp_other_nodes: "
       End
     End
