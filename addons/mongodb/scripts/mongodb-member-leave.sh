dd67b5a47588967feecc49850124762c52fe4ed3
gnolong <2391353625@qq.com>
2025-09-17 15:28:37 +0800

    fix

--- addons/mongodb/scripts/mongodb-member-leave.sh
+++ addons/mongodb/scripts/mongodb-member-leave.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-retry_count=0
-while [[ "$(timeout 5s /tools/syncerctl getrole)" == "primary" ]]; do
-    echo "current member role is primary, waiting for switchover done."
-    sleep 1
-    ((retry_count++))
-    if [ $retry_count -gt 60 ]; then
-        exit 1
-    fi
-done
+if [[ "$(timeout 5s /tools/syncerctl getrole)" == "primary" ]]; then
+    echo "current member role is primary."
+    exit 1
+fi
 
 /tools/syncerctl leave --instance "$KB_LEAVE_MEMBER_POD_NAME"
