200b0d0da2e145a885419c06698c053c2bd71a17
wangyelei <yelei.wang@apecloud.com>
2025-10-09 14:15:01 +0800

    feature: support hadoop and hive (#2031)
    Co-authored-by: wangyelei <wangyelei@users.noreply.github.com>
    (cherry picked from commit 92056465ca63a4ec694a9cee3e7c5d8737ce92b4)

---
+++ addons/hadoop/scripts/start-resourcemanager.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace # Uncomment this line for debugging purposes
+
+# Load libraries
+. /opt/scripts/libs/liblog.sh
+. /opt/scripts/libs/lib.sh
+. /kubeblocks/scripts/common.sh
+mkdir -p /hadoop/tmp
+mkdir -p /hadoop/yarn/resourcemanager
+
+info "** Starting Resource Manager **"
+exec ${HADOOP_HOME}/bin/yarn --config ${HADOOP_CONF_DIR} resourcemanager
