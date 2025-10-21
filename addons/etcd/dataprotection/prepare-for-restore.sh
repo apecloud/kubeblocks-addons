f292a950ec2cab2263b487025ac516518484dcee
loomt <loomt_@outlook.com>
2025-10-21 17:06:17 +0800

    chore[10]: delete file after etcd backup (#2141)

--- addons/etcd/dataprotection/prepare-for-restore.sh
+++ addons/etcd/dataprotection/prepare-for-restore.sh
@@ -3,9 +3,9 @@ set -exo pipefail
 
 export PATH="$PATH:$DP_DATASAFED_BIN_PATH"
 export DATASAFED_BACKEND_BASE_PATH="$DP_BACKUP_BASE_PATH"
-mkdir -p "$RESTORE_DIR"
+mkdir -p "$BACKUP_DIR"
 
 remote_backup_file="${DP_BACKUP_NAME}.tar.zst"
 if [ "$(datasafed list "$remote_backup_file")" = "$remote_backup_file" ]; then
-  datasafed pull -d zstd-fastest "$remote_backup_file" - | tar -xvf - -C "$RESTORE_DIR"
+  datasafed pull -d zstd-fastest "$remote_backup_file" - | tar -xvf - -C "$BACKUP_DIR"
 fi
