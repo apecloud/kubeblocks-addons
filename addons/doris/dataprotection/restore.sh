e1cbe6640471fe6bc06bbe1e3f4ffdb7b6f7261c
1aal <alal@infracreate.com>
2025-10-27 18:17:59 +0800

    chore

--- addons/doris/dataprotection/restore.sh
+++ addons/doris/dataprotection/restore.sh
@@ -5,27 +5,6 @@ export DATASAFED_BACKEND_BASE_PATH="$DP_BACKUP_BASE_PATH"
 
 SQL_CMD="mysql -N -B -h ${DP_DB_HOST}.${POD_NAMESPACE}.svc.cluster.local -P ${FE_QUERY_PORT} -u root -p${DP_DB_PASSWORD} -e"
 
-# Save backup status info file for syncing progress.
-# timeFormat: %Y-%m-%dT%H:%M:%SZ
-DP_save_backup_status_info() {
-    local totalSize=$1
-    local startTime=$2
-    local stopTime=$3
-    local timeZone=$4
-    local extras=$5
-    local timeZoneStr=""
-    if [ ! -z ${timeZone} ]; then
-       timeZoneStr=",\"timeZone\":\"${timeZone}\""
-    fi
-    if [ -z "${stopTime}" ];then
-      echo "{\"totalSize\":\"${totalSize}\"}" > ${DP_BACKUP_INFO_FILE}
-    elif [ -z "${startTime}" ];then
-      echo "{\"totalSize\":\"${totalSize}\",\"extras\":[${extras}],\"timeRange\":{\"end\":\"${stopTime}\"${timeZoneStr}}}" > ${DP_BACKUP_INFO_FILE}
-    else
-      echo "{\"totalSize\":\"${totalSize}\",\"extras\":[${extras}],\"timeRange\":{\"start\":\"${startTime}\",\"end\":\"${stopTime}\"${timeZoneStr}}}" > ${DP_BACKUP_INFO_FILE}
-    fi
-}
-
 DP_log() {
     msg=$1
     local curr_date=$(date -u '+%Y-%m-%d %H:%M:%S')
@@ -150,6 +129,7 @@ parse_datasafed_conf() {
     DP_log "BACKUP ID: $DP_BACKUP_ID"
 }
 
+
 prepare_repository_location() {
      local location=""
      if [[ "$DP_S3_ROOT" == s3://* ]]; then
@@ -209,71 +189,101 @@ cleanup_repository() {
     DP_log "Repository '$DP_DORIS_REPOSITORY' dropped successfully"
 }
 
-do_backup_and_wait() {
-    all_dbs=$($SQL_CMD "SHOW DATABASES" | grep -v "information_schema" | grep -v "mysql")
-    # DP_BACKUP_NAME=brier-5f7695dcfb-20251023071631    
-    for db in $all_dbs; do
-        $SQL_CMD "BACKUP SNAPSHOT $db.\`${db}_${DP_BACKUP_ID}\` TO $DP_DORIS_REPOSITORY" -D "$db"
-    done
-
-    wait_backup_complete "$all_dbs"
+do_snapshot_restore(){
+    snapshot_name=$1
+    db_name=$(echo "$snapshot_name" | awk -F'_' '{print $1}')
 }
 
-wait_backup_complete() {
+restore_backups(){
+    # show all snapshot
+    local all_dbs=""
+    $SQL_CMD "SHOW SNAPSHOT ON \`$DP_DORIS_REPOSITORY\`" | while read -r line; do
+        snapshot_name=$(echo "$line" | awk '{print $1}')
+        timestamp=$(echo "$line" | awk '{print $2}')
+        # ignore empty lines
+        if [ -z "$snapshot_name" ]; then
+            continue
+        fi
+
+        if [[ "$snapshot_name" != *_${DP_BACKUP_ID} ]]; then
+            DP_log "Skipping snapshot '$snapshot_name' as it does not belong to this backup."
+            continue
+        fi
+
+        if [[ "$snapshot_name" == "__internal_schema_"* ]]; then
+            DP_log "Skipping internal schema snapshot: $snapshot_name"
+            continue
+        fi
+
+        # Correctly parse db_name by removing the backup ID suffix
+        db_name=${snapshot_name%_${DP_BACKUP_ID}}        
+        DP_log "Creating database \`$db_name\` if not exists"
+        $SQL_CMD "CREATE DATABASE IF NOT EXISTS \`$db_name\`"
+        if [ $? -ne 0 ]; then
+            DP_log "Failed to create database \`$db_name\`, skip restore snapshot \`$snapshot_name\`"
+            continue
+        fi
+        local restore_sql
+        restore_sql="RESTORE SNAPSHOT \`$snapshot_name\` FROM \`$DP_DORIS_REPOSITORY\`"
+        restore_sql+=" PROPERTIES ("
+        restore_sql+="\"backup_timestamp\" = \"$timestamp\""
+        restore_sql+=");"
+        DP_log "Restoring snapshot \`$snapshot_name\` for database \`$db_name\`: $restore_sql"
+        if ! $SQL_CMD "$restore_sql" -D "$db_name"; then
+            DP_error_log "Failed to restore snapshot \`$snapshot_name\` for database \`$db_name\`"
+        fi
+        all_dbs+="$db_name "
+    done
+
+    all_dbs=$(echo "$all_dbs" | xargs)
+    wait_for_restore_complete "$all_dbs"
+}
+
+wait_for_restore_complete() {
     local max_wait_time=3600  # 1h
     local wait_interval=10    # 10s
     local elapsed_time=0
     local all_finished=false
-
-    DP_log "Wait for backup tasks to complete in $max_wait_time seconds..."
     local all_dbs=$1
-    DP_log "Backup databases: $all_dbs"
-   
+    DP_log "Wait for restore tasks to complete in $max_wait_time seconds..."
+    DP_log "Restore databases: $all_dbs"
+
+
     while [ $elapsed_time -lt $max_wait_time ]; do
-        all_finished=true
-        
+        all_finished=true        
         for db in $all_dbs; do
-            local backup_status
-            DP_log "Check backup status for database $db"
-            backup_status=$($SQL_CMD "SHOW BACKUP" -D "$db"  | grep "$db.$BACKUP_ID" | awk '{print $4}')            
-            if [ -z "$backup_status" ]; then
-                DP_log "Warning: no backup status found for database $db"
+            local restore_status
+            DP_log "Check restore status for database $db"
+            restore_status=$($SQL_CMD "SHOW RESTORE" -D "$db"  | grep "$db.$BACKUP_ID" | awk '{print $5}')            
+            if [ -z "$restore_status" ]; then
+                DP_log "Warning: no restore status found for database $db"
                 all_finished=false
-            elif [ "$backup_status" != "FINISHED" ]; then
-                DP_log "Backup task for database $db is not finished, status: $backup_status"
+            elif [ "$restore_status" != "FINISHED" ]; then
+                DP_log "Restore task for database $db is not finished, status: $restore_status"
                 all_finished=false
             else
-                DP_log "Backup task for database $db is finished"
+                DP_log "Restore task for database $db is finished"
             fi
         done
         
         if $all_finished; then
-            DP_log "All backup tasks are finished"
+            DP_log "All restore tasks are finished"
             return
         fi
         
-        DP_log "Wait $wait_interval seconds to check backup status again..."
+        DP_log "Wait $wait_interval seconds to check restore status again..."
         sleep $wait_interval
         elapsed_time=$((elapsed_time + wait_interval))
     done
     
-    DP_error_log "Error: Backup tasks timeout after $elapsed_time seconds"
+    DP_error_log "Error: Restore tasks timeout after $elapsed_time seconds"
 }
-
-backup_meta() {
-   tar -cvf - /opt/apache-doris/fe/doris-meta | datasafed push -z zstd-fastest - "${DP_BACKUP_NAME}.tar.zst"
-}
-
 
 main() {
-    start_time=$(date -u '+%Y-%m-%dT%H:%M:%SZ')
     parse_datasafed_conf
     prepare_s3_repository
-    do_backup_and_wait
+    restore_backups
     cleanup_repository
-    end_time=$(date -u '+%Y-%m-%dT%H:%M:%SZ')
-    total_size=$(datasafed stat / | grep 'TotalSize:' | awk '{print $2}')
-    DP_save_backup_status_info "${total_size:-0}" "$start_time" "$end_time" "" ""
 }
 
 main
