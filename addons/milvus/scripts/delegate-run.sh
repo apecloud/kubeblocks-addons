9815d41672b3e83d8284019fe742e892b5cec72b
Harold Cheng <niuchangcun@gmail.com>
2025-11-18 17:44:24 +0800

    fix: creation failed

--- addons/milvus/scripts/delegate-run.sh
+++ addons/milvus/scripts/delegate-run.sh
@@ -4,4 +4,4 @@ set -e
 
 sed "s/MINIO_ACCESS_KEY/${MINIO_ACCESS_KEY}/g; s/MINIO_SECRET_KEY/${MINIO_SECRET_KEY}/g" /milvus/configs/operator/user.yaml.raw > /milvus/configs/operator/user.yaml
 
-exec $@
+exec "$@"
