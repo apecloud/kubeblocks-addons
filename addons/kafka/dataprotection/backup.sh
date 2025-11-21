955d37f5975a053fd369695d31b4d890da856b92
Harold Cheng <niuchangcun@gmail.com>
2025-11-21 15:15:47 +0800

    handle some topic config

--- addons/kafka/dataprotection/backup.sh
+++ addons/kafka/dataprotection/backup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-readarray -t topics < <(kafkactl get topics -o compact)
-printf "%s\n" "${topics[@]}" | datasafed push - topics.txt
+# topics.txt format is like:
+# (topic name)             (partitions)   (replication factor)
+# topic1                   1              1
+# topic2                   1              1
+#
+# We also ignores the __consumer_offsets topic as offsets won't be backuped up.
+echo "getting topics..."
+kafkactl get topics | tail -n +2 | grep -v __consumer_offsets | datasafed push - topics.txt
+readarray -t topics < <(kafkactl get topics -o compact | grep -v  __consumer_offsets)
+
 for topic in "${topics[@]}"; do
+  echo "backing up ${topic}..."
   kafkactl consume "${topic}" --from-beginning --print-keys --print-timestamps --exit --print-headers -o json-raw | datasafed push - "data/${topic}.json"
 done
 
