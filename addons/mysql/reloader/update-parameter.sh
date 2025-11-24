4bbf6488dd132c552eed956bd7c0be8c1ae867d2
Lek <asd98041@qq.com>
2025-11-24 15:06:19 +0800

    fix: mysql reload failed (#2263)

--- addons/mysql/reloader/update-parameter.sh
+++ addons/mysql/reloader/update-parameter.sh
@@ -12,6 +12,28 @@ paramValue="${2:?missing value}"
 if echo "${paramName}" | grep -q "^loose_"; then
     paramName=$(echo "${paramName}" | sed 's/^loose_//')
 fi
+paramName=$(echo "${paramName}" | tr '-' '_')
 
-mysql_exec "SET GLOBAL ${paramName}=${paramValue};"
+var_int=-1
+if [[ "${paramValue}" =~ ^[0-9]+$ ]]; then
+    var_int="${paramValue}"
+fi
+if [ ${var_int} -lt 0 ]; then
+    if [[ "${paramValue}" =~ ^([0-9]+)(K|KB|k|kb)$ ]]; then
+        number="${BASH_REMATCH[1]}"
+        var_int=$((number * 1024))
+    elif [[ "${paramValue}" =~ ^([0-9]+)(M|MB|m|mb)$ ]]; then
+        number="${BASH_REMATCH[1]}"
+        var_int=$((number * 1024 * 1024))
+    elif [[ "${paramValue}" =~ ^([0-9]+)(G|GB|g|gb)$ ]]; then
+        number="${BASH_REMATCH[1]}"
+        var_int=$((number * 1024 * 1024 * 1024))
+    fi
+fi
+
+if [ ${var_int} -ge 0 ]; then
+    mysql_exec "SET GLOBAL ${paramName} = ${var_int};"
+else
+    mysql_exec "SET GLOBAL ${paramName} = '${paramValue}';"
+fi
 
