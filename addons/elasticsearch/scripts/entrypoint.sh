d8c6487f7ce632f32f6c0c0b924205b797aa39c3
Guo Ziang <ziang.gza@apecloud.com>
2025-11-27 16:07:13 +0800

    chore: tidy up

--- addons/elasticsearch/scripts/entrypoint.sh
+++ addons/elasticsearch/scripts/entrypoint.sh
@@ -17,29 +17,32 @@ if [ "${ZONE_AWARE_ENABLED}" = "true" ]; then
     IFS=':' read -ra ZONE_PART <<< "${PART}"
     if [ ${#ZONE_PART[@]} -eq 2 ]; then
       ZONE="${ZONE_PART[0]// /}"
+      ZONES+=("${ZONE}")
       NODES="${ZONE_PART[1]}"
       IFS=',' read -ra NODE_LIST <<< "${NODES}"
       for NODE in "${NODE_LIST[@]}"; do
         NODE_NAME_TRIMMED="${NODE// /}"
         if [ "${NODE_NAME_TRIMMED}" = "${NODE_NAME}" ]; then
           export CURRENT_ZONE="${ZONE}"
-          ZONES+=("${ZONE}")
           break
         fi
       done
-      if [ -n "${CURRENT_ZONE}" ]; then
-        break
-      fi
     fi
   done
   if [ -z "${CURRENT_ZONE}" ]; then
     echo "Error: ZONE_AWARE_ENABLED is true but failed to find zone for node ${NODE_NAME} in ZONE_AWARE_MAPPING: ${ZONE_AWARE_MAPPING}"
     exit 1
   fi
+  echo "CURRENT_ZONE: ${CURRENT_ZONE}"
+  # check if ZONES is empty
+  if [ ${#ZONES[@]} -eq 0 ]; then
+    echo "Error: ZONE_AWARE_ENABLED is true but failed to find zones in ZONE_AWARE_MAPPING: ${ZONE_AWARE_MAPPING}"
+    exit 1
+  fi
+  export ALL_ZONES=$(IFS=,; echo "${ZONES[*]}")
+  echo "ALL_ZONES: ${ALL_ZONES}"
 fi
-# join ZONES array with comma
-ALL_ZONES=$(IFS=,; echo "${ZONES[*]}")
-export ALL_ZONES
+
 # remove initial master nodes block if cluster has been formed
 if [ -f "${CLUSTER_FORMED_FILE}" ]; then
   sed -i '/# INITIAL_MASTER_NODES_BLOCK_START/,/# INITIAL_MASTER_NODES_BLOCK_END/d' config/elasticsearch.yml
