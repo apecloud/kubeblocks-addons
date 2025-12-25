d8ed45e06c262b7ae04a4d71808e295a6a87733b
wangyelei <yelei.wang@apecloud.com>
2025-12-25 10:03:25 +0800

    chore: remove tdengine (#2350)
    Co-authored-by: wangyelei <wangyelei@users.noreply.github.com>

--- addons/tdengine/scripts/reload-parameter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-paramName="${1:?missing param name}"
-paramValue="${2:?missing value}"
-res=$(taos -p$TAOS_ROOT_PASSWORD -s "alter all dnodes '${paramName} ${paramValue}'")
-if [[ "$res" == *"Query OK"* ]]; then
-  exit 0
-fi
-exit 1
