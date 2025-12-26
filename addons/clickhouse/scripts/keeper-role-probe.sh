1d1d9c1f80656b05ceb5e09151750e93c2e541f3
loomt <loomt_@outlook.com>
2025-12-26 11:58:54 +0800

    feat: improve ck sharding restore (#2339)
    Co-authored-by: loomts <loomts@users.noreply.github.com>

--- addons/clickhouse/scripts/keeper-role-probe.sh
+++ addons/clickhouse/scripts/keeper-role-probe.sh
@@ -2,12 +2,12 @@
 source /scripts/common.sh
 
 function get_keeper_role() {
-  local mode=$(get_mode 127.0.0.1)
-  if [ "$mode" == "standalone" ]; then
-    printf "leader"
-  elif [ "$mode" == "follower" ] || [ "$mode" == "leader" ] || [ "$mode" == "observer" ]; then
-    printf "%s" "$mode"
-  fi
+	local mode=$(get_mode 127.0.0.1)
+	if [ "$mode" == "standalone" ]; then
+		printf "leader"
+	elif [ "$mode" == "follower" ] || [ "$mode" == "leader" ] || [ "$mode" == "observer" ]; then
+		printf "%s" "$mode"
+	fi
 }
 
 get_keeper_role
