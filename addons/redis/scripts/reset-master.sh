3979ea9ed7161e4373bfbfc89d920c2c204c1f6b
wangyelei <yelei.wang@apecloud.com>
2026-01-04 17:34:34 +0800

    update ops def

--- addons/redis/scripts/reset-master.sh
+++ addons/redis/scripts/reset-master.sh
@@ -2,13 +2,17 @@
 if [ -z "${SENTINEL_POD_NAME_LIST}" ]; then
    exit 0
 fi
+sentinel_service_port=$SENTINEL_SERVICE_PORT
+if [ "$TLS_ENABLED" == "true" ]; then
+    sentinel_service_port=$SENTINEL_NON_TLS_SERVICE_PORT
+fi
 for sentinel_pod in $(echo ${SENTINEL_POD_NAME_LIST} | tr ',' '\n'); do
     echo "reset master in sentinel ${pod}..."
     fqdn="$sentinel_pod.$SENTINEL_HEADLESS_SERVICE_NAME.$CLUSTER_NAMESPACE.svc.cluster.local"
     if [ -n "${SENTINEL_PASSWORD}" ]; then
-        redis-cli -h $fqdn -p 26379 -a ${SENTINEL_PASSWORD} sentinel reset ${REDIS_COMPONENT_NAME}
+        redis-cli -h $fqdn -p $sentinel_service_port -a ${SENTINEL_PASSWORD} sentinel reset ${REDIS_COMPONENT_NAME}
     else
-        redis-cli -h $fqdn -p 26379 sentinel reset ${REDIS_COMPONENT_NAME}
+        redis-cli -h $fqdn -p $sentinel_service_port sentinel reset ${REDIS_COMPONENT_NAME}
     fi
     if [ $? -eq 0 ]; then
         echo "reset master in sentinel ${pod} succeeded"
