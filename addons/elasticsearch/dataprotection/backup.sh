322f7de680bb9f7fa8692d223a13b8372a52d5b1
kizuna-lek <asd98041@qq.com>
2026-02-10 16:13:45 +0800

    chore: support es dump

--- addons/elasticsearch/dataprotection/backup.sh
+++ addons/elasticsearch/dataprotection/backup.sh
@@ -11,19 +11,6 @@ toolConfig=/etc/datasafed/datasafed.conf
 REPOSITORY=kb-backup
 ES_ENDPOINT=http://${DP_DB_HOST}.${KB_NAMESPACE}.svc.cluster.local:9200
 
-# if the script exits with a non-zero exit code, touch a file to indicate that the backup failed,
-# the sync progress container will check this file and exit if it exists
-function handle_exit() {
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-    echo "failed with exit code $exit_code"
-    touch "${DP_BACKUP_INFO_FILE}.exit"
-    exit 1
-  else
-    echo "{}" >"${DP_BACKUP_INFO_FILE}"
-    exit 0
-  fi
-}
 trap handle_exit EXIT
 
 function getToolConfigValue() {
