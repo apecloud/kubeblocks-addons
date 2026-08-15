b2083b9154cf9744f5fbf8f2d1739800b17961e4
Amber <amber@slock.local>
2026-08-15 15:30:49 +0800

    fix(postgresql): fail pre-setup on initialization errors

--- addons/postgresql/scripts/postgres-pre-setup.sh
+++ addons/postgresql/scripts/postgres-pre-setup.sh
@@ -8,32 +8,32 @@ postgres_scripts_log_file="${postgres_log_dir}/scripts.log"
 postgres_walg_dir="/home/postgres/pgdata/wal-g"
 
 build_real_postgres_conf() {
-  mkdir -p "$postgres_conf_dir"
+  mkdir -p "$postgres_conf_dir" || return $?
 
   # Copy the template config file first
-  cp "$postgres_template_conf_file" "$postgres_conf_dir"
+  cp "$postgres_template_conf_file" "$postgres_conf_dir" || return $?
 
   # Set permissions
-  chmod 755 "$postgres_conf_dir"
-  chmod 664 "$postgres_conf_file"
+  chmod 755 "$postgres_conf_dir" || return $?
+  chmod 664 "$postgres_conf_file" || return $?
 }
 
 init_postgres_log() {
-  mkdir -p "$postgres_log_dir"
-  chmod -R 777 "$postgres_log_dir"
-  touch "$postgres_scripts_log_file"
-  chmod 666 "$postgres_scripts_log_file"
+  mkdir -p "$postgres_log_dir" || return $?
+  chmod -R 777 "$postgres_log_dir" || return $?
+  touch "$postgres_scripts_log_file" || return $?
+  chmod 666 "$postgres_scripts_log_file" || return $?
 }
 
 copy_necessary_binaries() {
-  mkdir -p "$postgres_walg_dir"
-  cp /spilo-init/bin/wal-g ${postgres_walg_dir}/wal-g
+  mkdir -p "$postgres_walg_dir" || return $?
+  cp /spilo-init/bin/wal-g "${postgres_walg_dir}/wal-g" || return $?
 }
 
 main() {
-  build_real_postgres_conf
-  init_postgres_log
-  copy_necessary_binaries
+  build_real_postgres_conf || return $?
+  init_postgres_log || return $?
+  copy_necessary_binaries || return $?
 }
 
 # This is magic for shellspec ut framework.
