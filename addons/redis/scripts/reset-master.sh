e76192a8a4061b3619ee6a39b3bd94c008bef5f6
wangyelei <yelei.wang@apecloud.com>
2026-08-28 21:45:00 +0800

    chore: support sentinel custom master name for redis reset master

--- addons/redis/scripts/reset-master.sh
+++ addons/redis/scripts/reset-master.sh
@@ -2,14 +2,16 @@
 if [ -z "${SENTINEL_POD_NAME_LIST}" ]; then
    exit 0
 fi
+${__SOURCED__:+false} : || return 0
+master_name=${CUSTOM_SENTINEL_MASTER_NAME:-$REDIS_COMPONENT_NAME}
 sentinel_service_port=${SENTINEL_SERVICE_PORT:-26379}
 for sentinel_pod in $(echo ${SENTINEL_POD_NAME_LIST} | tr ',' '\n'); do
     echo "reset master in sentinel ${pod}..."
     fqdn="$sentinel_pod.$SENTINEL_HEADLESS_SERVICE_NAME.$CLUSTER_NAMESPACE.svc.cluster.local"
     if [ -n "${SENTINEL_PASSWORD}" ]; then
-        redis-cli $REDIS_CLI_TLS_CMD -h $fqdn -p $sentinel_service_port -a ${SENTINEL_PASSWORD} sentinel reset ${REDIS_COMPONENT_NAME}
+        redis-cli $REDIS_CLI_TLS_CMD -h $fqdn -p $sentinel_service_port -a ${SENTINEL_PASSWORD} sentinel reset ${master_name}
     else
-        redis-cli $REDIS_CLI_TLS_CMD -h $fqdn -p $sentinel_service_port sentinel reset ${REDIS_COMPONENT_NAME}
+        redis-cli $REDIS_CLI_TLS_CMD -h $fqdn -p $sentinel_service_port sentinel reset ${master_name}
     fi
     if [ $? -eq 0 ]; then
         echo "reset master in sentinel ${pod} succeeded"
